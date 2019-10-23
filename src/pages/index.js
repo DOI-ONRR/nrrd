c64d1611db9220efc1ec80e7dd7efe8df17a9679
Mojo Nichols <mojo.nichols@onrr.gov>
2019-10-23 13:47:01 -0600

    Added bold new design

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your New  Gatsby site.</p>
+    <p>Welcome to your New <b>New</b>  Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
