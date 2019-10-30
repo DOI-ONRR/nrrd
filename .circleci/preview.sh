a1ad536cd78ba76a8b4d9ddf54509f6583a09953
Mojo Nichols <mojo.nichols@onrr.gov>
2019-10-30 07:59:28 -0600

    Added script for preview

---
+++ .circleci/preview.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+BRANCH=$1
+#BRANCH=`git rev-parse --abbrev-ref HEAD`
+#curl -v -L -o cf-cli_amd64.deb 'https://cli.run.pivotal.io/stable?release=debian64&source=github'
+#sudo dpkg -i cf-cli_amd64.deb
+PREVIEWS=`cf apps | grep started  | cut -d " " -f 1`
+count=0
+CANDIDATE="";
+for ii in $PREVIEWS
+do
+    echo $ii;
+    count=$((count+1))
+    if [ $ii != "onrr" ] && [ $ii != "dev-onrr" ]
+    then 
+	CANDIDATE=$ii
+	if [ $ii == $BRANCH ]
+	then 
+	    echo "echo exit loop"
+	    break
+	fi
+    else
+	echo "->> $ii"
+    fi
+     
+done
+echo $count
+if [ "$count" -gt 4 ]
+then
+    echo "cf delete -f $CANDIDATE"
+    echo "cf delete-route -f app.cloud.gov --hostname $CANDIDATE"
+    cf delete -f $CANDIDATE
+    cf delete-route -f app.cloud.gov --hostname $CANDIDATE
+fi    
+## Set $CF_USERNAME and $CF_PASSWORD in CircleCI settings.
+## $CF_ORG, $CF_SPACE, and $APP_NAME can also be set in CircleCI settings or hardcoded here.
+
+echo 'cf api https://api.fr.cloud.gov'
+echo 'cf auth "$CF_USERNAME" "$CF_PASSWORD"'
+echo 'cf target -o "$CF_ORG" -s "$CF_SPACE"'
+echo "cf push $BRANCH -f ./manifest.yml "
+
+cf api https://api.fr.cloud.gov
+cf auth "$CF_USERNAME" "$CF_PASSWORD"
+cf target -o "$CF_ORG" -s "$CF_SPACE"
+cf push $BRANCH -f ./manifest.yml   
