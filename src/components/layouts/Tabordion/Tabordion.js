1cfb0846866a08b1c1bf250c28bd58ba6919e378
Mojo Nichols <mojo.nichols@onrr.gov>
2019-12-06 16:12:00 -0700

    changed import from @material-ui/styles to @material-ui/core/styles as we only need @material-ui/styles if we want to use it independently from MUI.   I assume we don't but it caused build error.  I think other option is to add @material-ui/styles to package json.

--- src/components/layouts/Tabordion/Tabordion.js
+++ src/components/layouts/Tabordion/Tabordion.js
@@ -8,7 +8,7 @@ import Box from "@material-ui/core/Box"
 import Paper from "@material-ui/core/Paper"
 import Tabs from "@material-ui/core/Tabs"
 import Tab from "@material-ui/core/Tab"
-import { makeStyles, useTheme } from "@material-ui/styles"
+import { makeStyles, useTheme } from "@material-ui/core/styles"
 
 import ExploreData from "../../sections/ExploreData"
 
