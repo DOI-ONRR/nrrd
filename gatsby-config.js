a0ea763fba98001f53de155a3788736a7a8e153d
Jerome <jerome.louis@onrr.gov>
2020-01-07 15:59:36 -0700

    comment out pwa test for now

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,125 +1,125 @@
-const fetch = require("isomorphic-fetch");
-const { createHttpLink } = require("apollo-link-http");
+const fetch = require('isomorphic-fetch')
+const { createHttpLink } = require('apollo-link-http')
 
 // Federalist provides the BASEURL env variable for preview builds.
 // https://github.com/18F/federalist-garden-build#variables-exposed-during-builds
-const BASEURL = process.env.BASEURL || undefined;
+const BASEURL = process.env.BASEURL || undefined
 
 // Federalist provides the google_analytics env variable
 const GOOGLE_ANALYTICS_ID = process.env.google_analytics
   ? process.env.google_analytics[process.env.BRANCH] ||
     process.env.google_analytics.default
-  : "UA-33523145-1";
+  : 'UA-33523145-1'
 
 const config = {
   siteMetadata: {
-    title: "Natural Resources Revenue Data",
+    title: 'Natural Resources Revenue Data',
     description:
-      "This site provides open data about natural resource management on federal lands and waters in the United States, including oil, gas, coal, and other extractive industries.",
-    version: "v5.4.7",
+      'This site provides open data about natural resource management on federal lands and waters in the United States, including oil, gas, coal, and other extractive industries.',
+    version: 'v5.4.7',
     googleAnalyticsId: GOOGLE_ANALYTICS_ID,
-    author: "",
+    author: '',
     dataRetrieval: {
-      name: "Data Specialists",
-      email: "onrrdatarequests@onrr.gov"
+      name: 'Data Specialists',
+      email: 'onrrdatarequests@onrr.gov'
     },
     informationDataManagement: {
-      name: "Information and Data Management",
-      street: "1849 C Street NW MS 5134",
-      city: "Washington, D.C.",
-      zip: "20240",
-      email: "nrrd@onrr.gov"
+      name: 'Information and Data Management',
+      street: '1849 C Street NW MS 5134',
+      city: 'Washington, D.C.',
+      zip: '20240',
+      email: 'nrrd@onrr.gov'
     },
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: "Natural Resources Revenue Data",
-        short_name: "NRRD",
-        start_url: "/",
-        background_color: "#ffffff",
-        theme_color: "#663399",
+        name: 'Natural Resources Revenue Data',
+        short_name: 'NRRD',
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#663399',
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: "standalone",
-        icon: "src/img/favicon-32x32.png", // This path is relative to the root of the site.
+        display: 'standalone',
+        icon: 'src/img/favicon-32x32.png', // This path is relative to the root of the site.
         // An optional attribute which provides support for CORS check.
         // If you do not provide a crossOrigin option, it will skip CORS for manifest.
         // Any invalid keyword or empty string defaults to `anonymous`
-        crossOrigin: `use-credentials`,
+        crossOrigin: 'use-credentials',
       },
     },
     {
-      resolve: `gatsby-plugin-layout`,
+      resolve: 'gatsby-plugin-layout',
       options: {
-        component: `${__dirname}/src/components/layouts/DefaultLayout`
+        component: `${ __dirname }/src/components/layouts/DefaultLayout`
       }
     },
     {
-      resolve: `gatsby-plugin-material-ui`,
+      resolve: 'gatsby-plugin-material-ui',
       options: {
         // stylesProvider: {
         //   injectFirst: true,
         // }
       }
     },
-    `gatsby-theme-apollo`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-mdx`,
+    'gatsby-theme-apollo',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-mdx',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/src/pages`,
-        name: "pages"
+        path: `${ __dirname }/src/pages`,
+        name: 'pages'
       }
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `markdown-pages`,
-        path: `${__dirname}/src/markdown/`
+        name: 'markdown-pages',
+        path: `${ __dirname }/src/markdown/`
       }
     },
 
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/img`
+        name: 'images',
+        path: `${ __dirname }/src/img`
       }
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/img/gatsby-icon.png` // This path is relative to the root of the site.
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/img/gatsby-icon.png' // This path is relative to the root of the site.
       }
     },
     {
-      resolve: `gatsby-source-graphql`,
+      resolve: 'gatsby-source-graphql',
       options: {
-        typeName: `hasura`,
-        fieldName: `onrr`,
+        typeName: 'hasura',
+        fieldName: 'onrr',
         createLink: () => {
           return createHttpLink({
-            uri: "https://hasura-onrr.app.cloud.gov/v1/graphql",
+            uri: 'https://hasura-onrr.app.cloud.gov/v1/graphql',
             headers: {},
             fetch,
             resolvers: {}
-          });
+          })
         }
       }
     },
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
         // CommonMark mode (default: true)
         commonmark: true,
@@ -134,10 +134,10 @@ const config = {
       }
     },
     {
-      resolve: `@gatsby-contrib/gatsby-plugin-elasticlunr-search`,
+      resolve: '@gatsby-contrib/gatsby-plugin-elasticlunr-search',
       options: {
         // Fields to index
-        fields: [`title`, `tags`],
+        fields: ['title', 'tags'],
         // How to resolve each field's value for a supported node type
         resolvers: {
           // For any node of type MarkdownRemark, list how to resolve the fields' values
@@ -146,24 +146,24 @@ const config = {
             tags: node => node.frontmatter.tag || node.frontmatter.tags,
             path: node =>
               node.frontmatter.unique_id
-                ? "/explore/" + node.frontmatter.unique_id + "/"
+                ? '/explore/' + node.frontmatter.unique_id + '/'
                 : node.frontmatter.permalink
           }
         },
         // Optional filter to limit indexed nodes
-        filter: (node, getNode) => node.frontmatter.tags !== "exempt"
+        filter: (node, getNode) => node.frontmatter.tags !== 'exempt'
       }
     },
 
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-meta-redirect` // make sure to put last in the array
+    // 'gatsby-plugin-offline',
+    'gatsby-plugin-meta-redirect' // make sure to put last in the array
   ]
-};
-
-if (BASEURL) {
-  config.pathPrefix = `${BASEURL}`;
 }
 
-module.exports = config;
+if (BASEURL) {
+  config.pathPrefix = `${ BASEURL }`
+}
+
+module.exports = config
