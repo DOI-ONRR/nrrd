d93feacb6ca3fb1b11aa0f3cd324d61ebe52cc72
Jerome <jerome.louis@onrr.gov>
2020-01-08 15:33:55 -0700

    add chart child styling using color from theme provider

--- src/components/data-viz/StackedBarChart/StackedBarChart.js
+++ src/components/data-viz/StackedBarChart/StackedBarChart.js
@@ -6,70 +6,57 @@ import utils from '../../../js/utils'
 
 import Grow from '@material-ui/core/Grow'
 import { makeStyles } from '@material-ui/core/styles'
-//import stackedBarChart from '../../../js/bar-charts/stacked-bar-chart'
-import stackedBarChart from './stacked-bar-chart.js'
+// import stackedBarChart from '../../../js/bar-charts/stacked-bar-chart'
+import sBarChart from './stacked-bar-chart.js'
 
 const useStyles = makeStyles(theme => ({
-    chart: {
-	display:'block',
-	top:0,		    
-	left:0,
-	width: '100%',
-	height: '200px',
-	fill: '#435159',
-
+  chart: {
+    display: 'block',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '200px',
+    fill: '#435159',
+    '& .bars > .stacked-bar-chart-bar': {
+      fill: theme.palette.chart.secondary
     },
-	'& .selected': {
-		fill: '#086996'
-	},
-    legend: {
-	display:'block',
-	top:0,		    
-	left:0,
-	width: '100%',
-		height: '200px',
-	    fill: '#435159'
-    }
+  },
+  legend: {
+    display: 'block',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '200px',
+	  fill: '#435159'
+  }
 }))
 
-const StackedBarChart = (props) => {
-    // const mapJson=props.mapJson || "https://cdn.jsdelivr.net/npm/us-atlas@2/us/10m.json";
-    //use ONRR topojson file for land
+const StackedBarChart = props => {
+  // const mapJson=props.mapJson || "https://cdn.jsdelivr.net/npm/us-atlas@2/us/10m.json";
+  // use ONRR topojson file for land
 
-    const classes = useStyles();
-    const data=props.data;
-    const selected=props.selected;
-    const elemRef = useRef(null);
-    
-    useEffect( () => {
-	console.debug(data);
-	console.debug("________________________________________________");
-	
-	//	stackedBarChar(elemRef.current,{}, datas);
-	elemRef.current.children[0].innerHTML='';
-	elemRef.current.children[1].innerHTML='';
-	let chart= new stackedBarChart(elemRef.current,  data);
-	//chart.selected(selected);
-	chart.draw(data);
+  const classes = useStyles()
+  const data = props.data
+  const selected = props.selected
+  const elemRef = useRef(null)
 
   useEffect(() => {
     console.debug(data)
     console.debug('________________________________________________')
 
-    // stackedBarChar(elemRef.current,{}, data);
-    const chart = new D3StackedBarChart(elemRef.current, data)
-    chart.selected(selected)
+    // stackedBarChar(elemRef.current,{}, datas);
+    elemRef.current.children[0].innerHTML = ''
+    elemRef.current.children[1].innerHTML = ''
+    const chart = new sBarChart(elemRef.current, data)
+    // chart.selected(selected);
     chart.draw(data)
-
-    // stackedBarChart.create(elemRef.current,{sortOrder: ["Y1","Y2","Y3", "Y4"]}, data);
   })
 
   return (
-	    <div className={classes.chart} ref={elemRef} >
-	    <div className={classes.chart}></div><div className={classes.legend}> </div>
-        </div>
-  	)
+	  <div className={classes.chart} ref={elemRef}>
+	    <div className={classes.chart}></div><div className={classes.legend}></div>
+    </div>
+  )
 }
 
 export default StackedBarChart
-
