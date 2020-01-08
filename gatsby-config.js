f31b31fd0280f3982be93b59bd0ae33024ad1d15
Jeffery Keene <Jeff.Keene@master12studio.com>
2020-01-08 17:36:11 -0500

    removed markdown plugin and templates

--- gatsby-config.js
+++ gatsby-config.js
@@ -119,21 +119,6 @@ const config = {
       }
     },
     {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        // CommonMark mode (default: true)
-        commonmark: true,
-        // Footnotes mode (default: true)
-        footnotes: true,
-        // Pedantic mode (default: true)
-        pedantic: true,
-        // GitHub Flavored Markdown mode (default: true)
-        gfm: true,
-        // Plugins configs
-        plugins: []
-      }
-    },
-    {
       resolve: `@gatsby-contrib/gatsby-plugin-elasticlunr-search`,
       options: {
         // Fields to index
