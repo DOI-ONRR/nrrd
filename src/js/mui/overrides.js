5688825c0dc0de175040c1d6ee58a0470cddea6a
Jerome <jerome.louis@onrr.gov>
2020-01-16 17:14:46 -0700

    Updates to tabordion to be tabtastic

--- src/js/mui/overrides.js
+++ src/js/mui/overrides.js
@@ -77,7 +77,8 @@ module.exports = Object.freeze({
     root: {
       background: '#f0f6fa',
       borderTop: '5px solid #f0f6fa',
-      marginLeft: '10px',
+      marginLeft: '5px',
+      marginRight: '5px',
       color: '#435159',
       '& span:hover': {
         textDecoration: 'underline',
