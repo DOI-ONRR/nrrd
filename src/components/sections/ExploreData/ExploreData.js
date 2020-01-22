d5653c8958298a1be700356cf58968f178067a5f
Mojo Nichols <mojo.nichols@onrr.gov>
2020-01-22 16:21:31 -0700

    Removed pre cache

--- src/components/sections/ExploreData/ExploreData.js
+++ src/components/sections/ExploreData/ExploreData.js
@@ -210,16 +210,13 @@ const ExploreData = () => {
     dispatch({ type: 'CARDS', payload: { cards: cards.filter(item => item.fips !== fips) } })
   }
 
-  let { loading, error, data } = useQuery(FISCAL_REVENUE_QUERY, {
+  const { loading, error, data } = useQuery(FISCAL_REVENUE_QUERY, {
     variables: { year, location }
   })
- const cache = [2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
-
-  cache.map((year, i) => {
-      useQuery(FISCAL_REVENUE_QUERY, { variables: { year, location } })
-  })
-
-
+  // const cache = [2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
+  //  cache.map((year, i) => {
+  //      useQuery(FISCAL_REVENUE_QUERY, { variables: { year, location } })
+  //  })
 
   if (loading) {
     return loading
