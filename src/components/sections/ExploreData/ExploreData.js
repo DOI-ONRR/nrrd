710d4599a7f8b3d6bca4e802280fee7090e9b466
Mojo Nichols <mojo.nichols@onrr.gov>
2020-01-22 16:13:52 -0700

    use pre cache

--- src/components/sections/ExploreData/ExploreData.js
+++ src/components/sections/ExploreData/ExploreData.js
@@ -213,11 +213,11 @@ const ExploreData = () => {
   let { loading, error, data } = useQuery(FISCAL_REVENUE_QUERY, {
     variables: { year, location }
   })
- // const cache = [2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
+ const cache = [2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
 
- //  cache.map((year, i) => {
- //      useQuery(FISCAL_REVENUE_QUERY, { variables: { year, location } })
- //  })
+  cache.map((year, i) => {
+      useQuery(FISCAL_REVENUE_QUERY, { variables: { year, location } })
+  })
 
 
 
