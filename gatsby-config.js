6d39270a6097643334f93ec2b74718918ba479d0
Jeffery Keene <Jeff.Keene@master12studio.com>
2020-02-24 17:21:17 -0500

    updated favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,7 @@ const config = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: 'minimal-ui',
-        icon: 'src/images/icons/favicon-32x32.png', // This path is relative to the root of the site.
+        icon: 'src/images/icons/favicon.png', // This path is relative to the root of the site.
         // An optional attribute which provides support for CORS check.
         // If you do not provide a crossOrigin option, it will skip CORS for manifest.
         // Any invalid keyword or empty string defaults to `anonymous`
@@ -109,18 +109,6 @@ const config = {
     },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/img/gatsby-icon.png' // This path is relative to the root of the site.
-      }
-    },
     {
       resolve: 'gatsby-source-graphql',
       options: {
