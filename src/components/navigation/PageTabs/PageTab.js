853cffb7aadacb5495948d3d4a57bf6508231bae
Jeffery Keene <Jeff.Keene@master12studio.com>
2020-03-04 14:43:31 -0500

    finished process pages

--- src/components/navigation/PageTabs/PageTab.js
+++ src/components/navigation/PageTabs/PageTab.js
@@ -7,7 +7,7 @@ export default ({ children, to, ...rest }) => {
   return (
     <Box bgcolor={to ? 'white' : 'primary.main'}>
       <Button {...rest} disableElevation={true} variant='text' onClick={() => (to && (window.location.href = to))}>
-        <Box pl={2} pr={2}>{children}</Box>
+        <Box pl={2} pr={2} style={{ textTransform: 'none' }}>{children}</Box>
       </Button>
     </Box>
   )
