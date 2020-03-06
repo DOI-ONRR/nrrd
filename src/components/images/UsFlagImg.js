b6863bc013ee973d60b0f4af0188394c717d6b5d
Jeffery Keene <Jeff.Keene@master12studio.com>
2020-03-06 15:36:01 -0500

    fixed bug

--- src/components/images/UsFlagImg.js
+++ src/components/images/UsFlagImg.js
@@ -1,26 +1,4 @@
 import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
-import Img from 'gatsby-image'
-/*
- * This component is built using `gatsby-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `useStaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
- * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.dev/gatsby-image
- * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import usFlagLogoPng from '../../images/icons/us-flag-small.png'
 
-export default ({ alt, ...rest }) => {
-  const data = useStaticQuery(graphql`
-    query {
-      imageSharp(fixed: {originalName: {eq: "us-flag-small.png"}}) {
-        fixed(width: 16) {
-          ...GatsbyImageSharpFixed
-        }
-      }
-    }
-  `)
-  return <Img fixed={data.imageSharp.fixed} alt={alt || 'US Flag'} {...rest} />
-}
+export default ({ alt, ...rest }) => <img src={usFlagLogoPng} alt={alt || 'US Flag'} {...rest} />
