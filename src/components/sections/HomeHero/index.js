259c3907ad4d84f14be891bfcf8ff431645abf20
Jerome <jerome.louis@onrr.gov>
2020-03-11 14:58:16 -0600

    update index and explore mdx files to be markdown centric

--- src/components/sections/HomeHero/index.js
+++
@@ -1,1 +0,0 @@
-export { default as HomeHero } from "./HomeHero"
--- src/components/sections/RevenueTrends/RevenueTrends.js
+++ src/components/sections/RevenueTrends/RevenueTrends.js
@@ -7,15 +7,16 @@ import utils from '../../../js/utils'
 import PercentDifference from '../../utils/PercentDifference'
 
 import { makeStyles } from '@material-ui/core/styles'
-import Box from '@material-ui/core/Box'
-import Typography from '@material-ui/core/Typography'
-import Table from '@material-ui/core/Table'
-import TableBody from '@material-ui/core/TableBody'
-import TableCell from '@material-ui/core/TableCell'
-import TableContainer from '@material-ui/core/TableContainer'
-import TableHead from '@material-ui/core/TableHead'
-import TableRow from '@material-ui/core/TableRow'
-import Paper from '@material-ui/core/Paper'
+import {
+  Box,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper
+} from '@material-ui/core'
 
 import Sparkline from '../../data-viz/Sparkline'
 
