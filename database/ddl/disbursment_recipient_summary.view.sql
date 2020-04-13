c464fb9b38423da35f174050ba04b074c778c829
Mojo Nichols <mojo.nichols@onrr.gov>
2020-04-13 12:21:01 -0600

    Added Disbursment map and disbursement recipient summary

--- database/ddl/disbursment_recipient_summary.view.sql
+++ database/ddl/disbursment_recipient_summary.view.sql
@@ -1,36 +1,54 @@
+drop view  "public"."disbursement_recipient_summary"; 
 CREATE OR REPLACE VIEW "public"."disbursement_recipient_summary" AS 
+( SELECT 'Nationwide Federal'::character varying AS state_or_area,
+        CASE
+            WHEN ((commodity.fund_type)::text ~~ 'State%'::text) THEN 'State and Counties'::text
+            WHEN ((commodity.fund_type)::text ~~ 'U.S.%'::text) THEN 'U.S. Treasury'::text
+            ELSE 'Specific Funds'::text
+        END AS recipient,
+    'Nationwide Federal'::text AS location,
+    fiscal_year,
+    calendar_year,
+    sum(disbursement.disbursement) AS total
+   FROM (((disbursement
+     JOIN commodity USING (commodity_id))
+     JOIN period USING (period_id))
+     JOIN location USING (location_id))
+  WHERE ((period.period)::text = 'Fiscal Year'::text)
+  GROUP BY
+        CASE
+            WHEN ((commodity.fund_type)::text ~~ 'State%'::text) THEN 'State and Counties'::text
+            WHEN ((commodity.fund_type)::text ~~ 'U.S.%'::text) THEN 'U.S. Treasury'::text
+            ELSE 'Specific Funds'::text
+        END, 'Nationwide Federal'::text,
+          fiscal_year,
+            calendar_year
 
-(
-select
-      
-       'Nationwide Federal' as state_or_area,
-case when  fund_type like 'State%' then 'State and Counties'
-       when fund_type like 'U.S.%' then 'U.S. Treasury'
-       else 'Specific Funds'
-      END as recipient,
-      'Nationwide Federal' as location,
-       sum(disbursement.disbursement) as t
-from disbursement  join commodity using (commodity_id) join period using (period_id) join location using (location_id)
-where period='Fiscal Year'
-group by fund, location
-order by t desc
-
-  )
+  ORDER BY (sum(disbursement.disbursement)) DESC)
 UNION
-(
-  select state as state_or_area,
+( SELECT location.state AS state_or_area,
+        CASE
+            WHEN (((location.state_name)::text <> ''::text) AND ((location.county)::text = ''::text)) THEN 'States'::text
+            WHEN (((location.state_name)::text <> ''::text) AND ((location.county)::text <> ''::text)) THEN 'Counties'::text
+            ELSE NULL::text
+        END AS recipient,
+    'State'::text AS location,
+            fiscal_year,
+            calendar_year,
 
-     case when state_name !='' and  county = '' then 'States'
-      when state_name !='' and county !='' then 'Counties'
-      end as  recipient,
-      case when state_name !='' and  county = '' then 'States'
-      when state_name !='' and county !='' then 'Counties'
-      end as location,
-       sum(disbursement.disbursement) as t
-from disbursement  join commodity using (commodity_id) join period using (period_id) join location using (location_id)
-where period='Fiscal Year'
-and state !=''
-group by state, fund, location
-order by t desc
+sum(disbursement.disbursement) AS total
+   FROM (((disbursement
+     JOIN commodity USING (commodity_id))
+     JOIN period USING (period_id))
+     JOIN location USING (location_id))
+  WHERE (((period.period)::text = 'Fiscal Year'::text) AND ((location.state)::text <> ''::text))
+  GROUP BY location.state,
+        CASE
+            WHEN (((location.state_name)::text <> ''::text) AND ((location.county)::text = ''::text)) THEN 'States'::text
+            WHEN (((location.state_name)::text <> ''::text) AND ((location.county)::text <> ''::text)) THEN 'Counties'::text
+            ELSE NULL::text
+        END, 'State'::text,
+          fiscal_year,
+            calendar_year
 
-)
+  ORDER BY (sum(disbursement.disbursement)) DESC);
---
+++ src/components/sections/ExploreData/Disbursements/DisbursementRecipientSummary.js
@@ -0,0 +1,186 @@
+import React, { useContext } from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+
+
+import Sparkline from '../../../data-viz/Sparkline'
+
+import utils from '../../../../js/utils'
+import { StoreContext } from '../../../../store'
+
+import {
+  Box,
+  Grid,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableRow,
+  Typography
+} from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+
+import CONSTANTS from '../../../../js/constants'
+
+const useStyles = makeStyles(theme => ({
+  table: {
+    width: '100%',
+    marginBottom: 0,
+    '& th': {
+      padding: 5,
+      lineHeight: 1
+    },
+    '& td': {
+      padding: 0,
+    },
+  },
+  paper: {
+    width: '100%'
+  },
+}))
+
+const APOLLO_QUERY = gql`
+  # summary card queries
+  query FiscalRevenue($year: Int!, $period: String!, $state: [String!]) {
+    cardFiscalDisbursementSummary: fiscal_disbursement_summary(
+      where: { state_or_area: { _in: $state } }
+      order_by: { fiscal_year: asc, state_or_area: asc }
+    ) {
+      fiscal_year
+      state_or_area
+      sum
+    }
+
+    cardDisbursementRecipientSummary: disbursement_recipient_summary(
+      limit: 3
+      where: { fiscal_year: { _eq: $year }, state_or_area: { _in: $state } }
+      order_by: { fiscal_year: asc, total: desc }
+    ) {
+      fiscal_year
+      recipient
+      state_or_area
+      total
+    }
+
+    cardDisbursementSparkdata: disbursement_recipient_summary(
+      where: { state_or_area: { _in: $state } }
+      order_by: { fiscal_year: asc }
+    ) {
+      fiscal_year
+      recipient 
+      total
+      state_or_area
+    }
+
+    # period query
+    period(where: {period: {_ilike: $period }}) {
+      fiscal_year
+    }
+  }
+`
+
+const DisbursementRecipientSummary = props => {
+
+  const { state } = useContext(StoreContext)
+  const classes = useStyles()
+  const year = state.year
+  console.debug("DT                ", state)
+const { loading, error, data } = useQuery(APOLLO_QUERY, {
+    variables: { state: props.abbr, year: year, period: CONSTANTS.FISCAL_YEAR }
+  })
+
+  if (loading) { return 'Loading ... ' }
+  if (error) return `Error! ${ error.message }`
+
+
+  let periodData
+
+  let distinctRecipients = 0
+  let topRecipients = []
+  let total = 0
+  if (
+    data &&
+    data.cardFiscalDisbursementSummary.length > 0 &&
+    data.cardDisbursementRecipientSummary.length > 0 &&
+    data.cardDisbursementSparkdata.length > 0
+  ) {
+    periodData = data.period
+
+    total = data.cardFiscalDisbursementSummary[data.cardFiscalDisbursementSummary.findIndex(x => x.fiscal_year === year)].sum
+    distinctRecipients = data.cardFiscalDisbursementSummary[data.cardFiscalDisbursementSummary.findIndex(x => x.fiscal_year === year)].distinct_commodities
+
+    topRecipients = data.cardDisbursementRecipientSummary
+      .map((item, i) => item.recipient)
+      .map((com, i) => {
+        const r = data.cardRecipientSparkdata.filter(item => item.recipient === com)
+        const s = r.map((row, i) => [row.fiscal_year, row.total])
+        const d = periodData.map((row, i) => {
+          const t = s.find(x => x[0] === row.fiscal_year)
+          return (
+            [row.fiscal_year, t ? t[1] : 0]
+          )
+        })
+        return { commodity: com, data: d }
+      })
+
+  return (
+      <>
+        <Grid container>
+          <Paper className={classes.paper} style={{ marginBottom: 10 }}>
+            <Table
+              className={classes.table}
+              size="small"
+              aria-label="top commodities table"
+              >
+              <TableBody>
+                {topCommodities &&
+                  topCommodities.map((row, i) => {
+                    return (
+                      <TableRow key={i}>
+                        <TableCell component="th" scope="row">
+                          <Typography style={{ fontSize: '.8rem' }}>
+                            {row.commodity}
+                          </Typography>
+                        </TableCell>
+                        <TableCell align="right">
+                          <Sparkline
+                            data={row.data}
+                            highlightIndex={row.data.findIndex(
+                              x => x[0] === year
+                            )}
+                            />
+                        </TableCell>
+                        <TableCell align="right">
+                          <Typography style={{ fontSize: '.8rem' }}>
+                            {utils.formatToSigFig_Dollar(
+                              Math.floor(
+                                // eslint-disable-next-line standard/computed-property-even-spacing
+                                topCommodities[i].data[
+                                  row.data.findIndex(x => x[0] === year)
+                                ][1]
+                              ),
+                              3
+                            )}
+                          </Typography>
+                        </TableCell>
+                      </TableRow>
+                    )
+                  })}
+      </TableBody>
+        </Table>
+        </Paper>
+        </Grid>
+
+        <Grid container>
+        <Grid item xs={12}>
+        <Typography variant="subtitle2" component="span">
+        Total Commodities: {distinctCommodities}
+      </Typography>
+        </Grid>
+        </Grid>
+        </>
+    )
+  }
+}
+
+export default DisbursementRecipientSummary
