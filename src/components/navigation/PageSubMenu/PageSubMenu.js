c928884122b6f5a49ff0ff0a7f21a4e041eb5517
Jerome <jerome.louis@onrr.gov>
2020-04-17 15:35:26 -0600

    updated styling for sticky year slider

--- src/components/navigation/PageSubMenu/PageSubMenu.js
+++ src/components/navigation/PageSubMenu/PageSubMenu.js
@@ -122,7 +122,7 @@ const PageSubMenu = ({ menuItems, ...props }) => {
 
   return (
     <Box className={classes.root}>
-      <StickyWrapper enabled={true} top={75} bottomBoundary={0} innerZ="1000" activeClass="sticky">
+      <StickyWrapper enabled={true} top={90} bottomBoundary={0} innerZ="1000" activeClass="sticky">
         <Paper elevation={1} square>
           <Container maxWidth="lg">
             <MenuList id="page-scrollto-subnav">
