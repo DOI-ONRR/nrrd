02c0b362310334daba765e5cfeefd13558f2e48f
Jeffery Keene <Jeff.Keene@master12studio.com>
2020-04-22 11:01:43 -0400

    added margin to footer

--- src/components/layouts/DefaultLayout/DefaultLayout.js
+++ src/components/layouts/DefaultLayout/DefaultLayout.js
@@ -6,13 +6,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { useStaticQuery, graphql } from 'gatsby'
 
-import { makeStyles, useTheme } from '@material-ui/core/styles'
-
-import {
-  CssBaseline,
-  Container,
-  Grid
-} from '@material-ui/core'
+import Box from '@material-ui/core/Box'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+import makeStyles from '@material-ui/core/styles/makeStyles'
+import useTheme from '@material-ui/core/styles/useTheme'
 
 import InfoBanner from '../../content-partials/InfoBanner'
 import Footer from '../../content-partials/Footer'
@@ -133,7 +132,9 @@ const DefaultLayout = ({ includeToc = true, children }) => {
         }
       </main>
       {data &&
-        <Footer data={data} />
+        <Box mt={3}>
+          <Footer data={data} />
+        </Box>
       }
     </React.Fragment>
   )
