9c307af28551e20ae50aa55cbd9f5cb5acf50403
Mojo Nichols <mojo.nichols@onrr.gov>
2020-04-27 15:34:15 -0600

    Set default view of explored data to revenue

--- src/components/sections/ExploreData/ExploreData.js
+++ src/components/sections/ExploreData/ExploreData.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, {useContext }  from 'react'
+import { DataFilterContext } from '../../../stores/data-filter-store'
+import { DATA_FILTER_CONSTANTS as DFC } from '../../../constants'
 
 const ExploreData = props => {
+  const { state: filterState, updateDataFilter } = useContext(DataFilterContext)
+  if(!filterState[DFC.DATA_TYPE])
+     updateDataFilter({ ...filterState, [DFC.DATA_TYPE]: 'Revenue' })
   return (
     <>
       {props.children}
