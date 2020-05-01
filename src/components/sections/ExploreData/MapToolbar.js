edf351f90f8401b268dbe4dcceb331d1a59e8c92
Jerome <jerome.louis@onrr.gov>
2020-05-01 11:39:13 -0600

    fix mobile explore more menu, update icon

--- src/components/sections/ExploreData/MapToolbar.js
+++ src/components/sections/ExploreData/MapToolbar.js
@@ -18,9 +18,8 @@ import {
 
 import ExploreIcon from '@material-ui/icons/Explore'
 import MapIcon from '@material-ui/icons/Map'
-import SearchIcon from '@material-ui/icons/Search'
-
 import MoreVertIcon from '@material-ui/icons/MoreVert'
+import SearchIcon from '@material-ui/icons/Search'
 
 import MapSelectControl from './MapSelectControl'
 import CONSTANTS from '../../../js/constants'
@@ -70,7 +69,7 @@ const useStyles = makeStyles(theme => ({
     right: 10,
     top: 4,
     zIndex: 99,
-    '@media (max-width: 768px)': {
+    '@media (max-width: 960px)': {
       position: 'relative',
       width: '100%',
       top: 'inherit',
@@ -258,7 +257,7 @@ const ExploreDataToolbar = props => {
   const [navValue, setNavValue] = useState(null)
   const [menu, setMenu] = useState({
     showMapTools: false,
-    showSearch: false,
+    // showSearch: false,
     showExplore: false,
   })
 
@@ -277,6 +276,7 @@ const ExploreDataToolbar = props => {
           <BottomNavigation
             value={navValue}
             onChange={(event, newValue) => {
+              console.log('newVal: ', newValue)
               setNavValue(newValue)
               switch (newValue) {
               case 0:
@@ -303,8 +303,11 @@ const ExploreDataToolbar = props => {
               }} />
             <BottomNavigationAction
               label="Explore more"
-              icon={<ExploreIcon />}
-              classes={{ root: classes.botNavRoot, selected: classes.botNavSelected }} />
+              icon={<MoreVertIcon />}
+              classes={{
+                root: classes.botNavRoot,
+                selected: classes.botNavSelected
+              }} />
           </BottomNavigation>
         </Box>
       </Hidden>
@@ -358,8 +361,12 @@ const ExploreDataToolbar = props => {
                 checkbox={false}
                 dataFilterType={DFC.COMMODITY} />
             }
-            {(menu.showExplore || matchesMdUp) &&
-              <Box className={classes.toolbarExploreMenu}>
+          </Box>
+        }
+
+        {(menu.showExplore || matchesMdUp) &&
+          <Box>
+
                 {dataType === REVENUE &&
                 <MapExploreMenu
                   linkLabels={['Query revenue data', 'Downloads & Documentation', 'How revenue works', 'Revenue by company']}
@@ -379,8 +386,6 @@ const ExploreDataToolbar = props => {
                 />
                 }
               </Box>
-            }
-          </Box>
         }
       </Box>
     </>
