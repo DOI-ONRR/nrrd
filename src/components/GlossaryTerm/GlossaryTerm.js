7a19b31c3fb8ed62bbabde46dcc80943bbaba974
Jerome <jerome.louis@onrr.gov>
2020-05-13 15:23:58 -0600

    resolve mui tooltip errors, correct homepage links

--- src/components/GlossaryTerm/GlossaryTerm.js
+++ src/components/GlossaryTerm/GlossaryTerm.js
@@ -67,8 +67,8 @@ const GlossaryTerm = ({ children, ...rest }) => {
   ))
 
   return (
-    <Tooltip title={termResults[0].definition} classes={styles} leaveDelay={200} arrow placement="top">
-      <TermDisplay tabindex='0' className={styles.term} {...rest} />
+    <Tooltip title={termResults[0].definition} classes={{ tooltip: styles.tooltip }} leaveDelay={200} arrow placement="top">
+      <TermDisplay tabIndex='0' className={styles.term} {...rest} />
     </Tooltip>
 
   )
