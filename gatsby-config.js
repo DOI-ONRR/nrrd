9bcfc38169709a275cc6d6fa9ff7b565ca51f7e1
Mojo Nichols <mojo.nichols@onrr.gov>
2020-05-20 12:26:19 -0600

    Merged 365-RevenueOverTimeChart  and pointing at sandbox

--- gatsby-config.js
+++ gatsby-config.js
@@ -125,7 +125,7 @@ const config = {
         createLink: () => {
           return createHttpLink({
             // uri: 'https://hasura-onrr.app.cloud.gov/v1/graphql',
-            uri: 'https://hasura-onrr.app.cloud.gov/v1/graphql',
+            uri: 'https://hasura-sandbox.app.cloud.gov/v1/graphql',
             headers: {},
             fetch,
             resolvers: {}
