12688a1c2bdcb4b014bd5bb2a7fe3913005c41eb
Jeffery Keene <Jeff.Keene@master12studio.com>
2020-06-03 13:01:28 -0400

    point to hasura-onrr

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -6,7 +6,7 @@ export const client = new ApolloClient({
 
   cache: new InMemoryCache(),
   link: new HttpLink({
-    uri: 'https://hasura-sandbox.app.cloud.gov/v1/graphql',
+    uri: 'https://hasura-onrr.app.cloud.gov/v1/graphql',
     headers: {},
     fetch,
     resolvers: {}
