fbdcd3a68e1b90649c0879c0a94b6113c80bf229
Jerome <jerome.louis@onrr.gov>
2020-06-12 10:06:55 -0600

    padd month lookup numbers for revenue and disbursement trends

--- src/components/sections/DisbursementTrends/DisbursementTrends.js
+++ src/components/sections/DisbursementTrends/DisbursementTrends.js
@@ -250,18 +250,18 @@ export default DisbursementTrends
 
 const monthLookup = month => {
   const monthNumber = {
-    January: 1,
-    February: 2,
-    March: 3,
-    April: 4,
-    May: 5,
-    June: 6,
-    July: 7,
-    August: 8,
-    September: 9,
-    October: 10,
-    November: 11,
-    December: 12
+    January: '01',
+    February: '02',
+    March: '03',
+    April: '04',
+    May: '05',
+    June: '06',
+    July: '07',
+    August: '08',
+    September: '09',
+    October: '10',
+    November: '11',
+    December: '12'
   }
 
   return monthNumber[month]
