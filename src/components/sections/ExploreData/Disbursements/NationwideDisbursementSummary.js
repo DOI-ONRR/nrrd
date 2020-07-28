06c5760483d3adde205e427e93b29d9b7734c23d
Jerome <jerome.louis@onrr.gov>
2020-07-28 11:28:11 -0600

    updates for nationwide disbursements summary

--- src/components/sections/ExploreData/Disbursements/NationwideDisbursementSummary.js
+++ src/components/sections/ExploreData/Disbursements/NationwideDisbursementSummary.js
@@ -46,9 +46,9 @@ const disbursementTypeDescriptions = [
   'Supports the establishment of critical infrastructure projects like dams and power plants.',
   // eslint-disable-next-line max-len
   'ONRR disburses 100% of revenue collected from resource extraction on Native American lands back to tribes, nations, and individuals.',
-  'Provides matching grants to states and local governments to buy and develop public outdoor recreation areas across the 50 states. How this fund works',
+  'Provides matching grants to states and local governments to buy and develop public outdoor recreation areas across the 50 states. <a href="/how-revenue-works/lwcf" style="color: #1478a6">How this fund works</a>',
+  'Helps preserve U.S. historical and archaeological sites and cultural heritage through grants to state and tribal historic preservation offices. <a href="/how-revenue-works/hpf" style="color: #1478a6">How this fund works</a>',
   'Some funds are directed back to federal agencies that administer these lands to help cover operational costs. The Ultra-Deepwater Research Program and the Mescal Settlement Agreement also receive $50 million each.',
-  'Helps preserve U.S. historical and archaeological sites and cultural heritage through grants to state and tribal historic preservation offices. How this fund works'
 ]
 
 const useStyles = makeStyles(theme => ({
@@ -80,6 +80,10 @@ const NationwideDisbursementSummary = props => {
   const units = 'dollars'
   const xGroups = {}
 
+  const createMarkup = markup => {
+    return { __html: markup }
+  }
+
   if (loading) {
     return 'Loading...'
   }
@@ -90,9 +94,9 @@ const NationwideDisbursementSummary = props => {
     // do something wit dat data
     console.log('NationwideDisbursementSummary data: ', data)
     groupData = utils.groupBy(data.fiscal_disbursement_recipient_source_summary, 'recipient')
-    console.debug("WTH", groupData)
-    
-    /*groupTotal = Object.keys(groupData).map(k =>
+    console.debug('WTH', groupData)
+
+    /* groupTotal = Object.keys(groupData).map(k =>
       groupData[k].reduce((sum, i) => {
         sum += i.total
       }, 0)).reduce((total, s) => {
@@ -101,7 +105,7 @@ const NationwideDisbursementSummary = props => {
     */
     groupTotal = Object.keys(groupData).map(k => groupData[k].reduce((sum, i) => sum += i.total, 0)).reduce((total, s) => total += s, 0)
     /*
-    // Have to keep long form with return (calc) for some reason. 
+    // Have to keep long form with return (calc) for some reason.
     groupTotal = Object.keys(groupData).map(k => {
       //console.debug("K: ", k, "groupData[k]", groupData[k] )
       let st= groupData[k].reduce((sum, i) => {
@@ -117,43 +121,42 @@ const NationwideDisbursementSummary = props => {
       }, 0)
     */
     nationwideSummaryData = Object.entries(groupData)
-  //  console.debug("groupTotal: ", groupTotal)
-/* debug    nationwideSummaryData.map((item, i) => {
+    //  console.debug("groupTotal: ", groupTotal)
+    /* debug    nationwideSummaryData.map((item, i) => {
       let barScale=item[1].reduce((sum, i) => { return (sum += i.total) }, 0) / groupTotal
       console.debug("barScale: ", barScale)
       console.debug("item: ", item)
     })
 */
-    
+    console.log('nationwideSummaryData: ', nationwideSummaryData)
 
+    return (
+      <Container id={utils.formatToSlug(title)}>
+        <Grid container>
+          <Grid item md={12}>
+            <Box color="secondary.main" mt={5} mb={2} borderBottom={2}>
+              <Box component="h3" color="secondary.dark">{title}</Box>
+            </Box>
+          </Grid>
+          <Grid item md={12}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell style={{ fontWeight: 'bold' }}>Recipient</TableCell>
+                  <TableCell style={{ fontWeight: 'bold' }}><span>Source</span>
+                    <span style={{ fontWeight: 'bold', float: 'right' }}>FY {year}</span></TableCell>
 
-  return (
-    <Container id={utils.formatToSlug(title)}>
-      <Grid container>
-        <Grid item md={12}>
-          <Box color="secondary.main" mt={5} mb={2} borderBottom={2}>
-            <Box component="h3" color="secondary.dark">{title}</Box>
-          </Box>
-        </Grid>
-        <Grid item md={12}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell style={{ fontWeight: 'bold' }}>Recipient</TableCell>
-                <TableCell style={{ fontWeight: 'bold' }}><span>Source</span>
-                  <span style={{ fontWeight: 'bold', float: 'right' }}>FY {year}</span></TableCell>
-
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              { nationwideSummaryData &&
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                { nationwideSummaryData &&
               nationwideSummaryData.map((item, i) => {
                 return (
                   <TableRow key={i}>
                     <TableCell style={{ verticalAlign: 'top' }}>
                       <Box component="h4" mt={0}>{item[0]}</Box>
                       <Box component="p">
-                        {disbursementTypeDescriptions[i]}
+                        <span dangerouslySetInnerHTML={createMarkup(disbursementTypeDescriptions[i])} />
                       </Box>
                     </TableCell>
                     <TableCell style={{ width: '65%' }}>
@@ -175,7 +178,7 @@ const NationwideDisbursementSummary = props => {
                           return headers
                         }
                         }
-                  barScale={item[1].reduce((sum, i) => sum += i.total, 0) / groupTotal }
+                        barScale={item[1].reduce((sum, i) => sum += i.total, 0) / groupTotal }
                         units={units}
                         xAxis={xAxis}
                         xLabels={xLabels}
@@ -189,14 +192,17 @@ const NationwideDisbursementSummary = props => {
                   </TableRow>
                 )
               })
-              }
-            </TableBody>
-          </Table>
+                }
+              </TableBody>
+            </Table>
+          </Grid>
         </Grid>
-      </Grid>
-    </Container>
-  )
-  } else {return(null) }
+      </Container>
+    )
+  }
+  else {
+    return (null)
+  }
 }
 
 export default NationwideDisbursementSummary
