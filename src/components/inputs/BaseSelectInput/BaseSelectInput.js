94a65c088b274f5b3b109980a365b4bbf4c5c1e4
Jerome <jerome.louis@onrr.gov>
2020-07-31 17:19:27 -0600

    update base select styling

--- src/components/inputs/BaseSelectInput/BaseSelectInput.js
+++ src/components/inputs/BaseSelectInput/BaseSelectInput.js
@@ -76,7 +76,6 @@ const BaseInput = withStyles(theme =>
       border: '1px solid #ced4da',
       padding: '8.5px 14px',
       transition: theme.transitions.create(['border-color', 'box-shadow']),
-      minWidth: 130,
       '&:focus': {
         borderRadius: 4,
         borderColor: '#80bdff',
