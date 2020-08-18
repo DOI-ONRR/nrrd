3f53950c4288f2bd5f64f12c26d3597dc0b74344
Jerome <jerome.louis@onrr.gov>
2020-08-18 08:57:36 -0600

    remove bg from 404 layout

--- src/components/layouts/NoPageFoundLayout/NoPageFoundLayout.js
+++ src/components/layouts/NoPageFoundLayout/NoPageFoundLayout.js
@@ -13,7 +13,6 @@ import Rig404 from '-!svg-react-loader!../../../img/svg/rig-404.svg'
 const useStyles = makeStyles(theme => (
   {
     root: {
-      backgroundColor: '#d3dfe6',
       color: '#435159',
       margin: '0',
       padding: '1.25em 1.875em',
