8200b3a6294a43c3753d6da317bbd60a4c8db982
Jerome <jerome.louis@onrr.gov>
2020-08-24 09:16:49 -0600

    decrease revenue grid template row height

--- src/components/sections/ExploreData/DetailCards.js
+++ src/components/sections/ExploreData/DetailCards.js
@@ -40,7 +40,7 @@ const useStyles = makeStyles(theme => ({
       maxWidth: '100%',
     },
     '& .cardContent__Revenue': {
-      gridTemplateRows: '185px 750px 850px',
+      gridTemplateRows: '185px 660px 750px',
     },
     '& .cardContent__Disbursements': {
       gridTemplateRows: '185px 855px 650px',
