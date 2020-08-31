2b94ff00972248f50c9c30e8c12bd33a56a2f265
Jerome <jerome.louis@onrr.gov>
2020-08-31 15:53:28 -0600

    remove seo favicon links since gatsby handles this

--- src/components/seo.js
+++ src/components/seo.js
@@ -73,9 +73,6 @@ function SEO ({ description, lang, meta, title }) {
           },
         ].concat(meta)}
       >
-        <link rel="icon" type="image/x-icon" href={withPrefix('/img/favicon.ico')} />
-        <link rel="icon" type="image/x-icon" href={withPrefix('/img/favicon-16x16.png')} sizes="16x16" />
-        <link rel="icon" type="image/x-icon" href={withPrefix('/img/favicon-32x32.png')} sizes="32x32" />
 
         {/* Digital Analytics Program roll-up, see the data at https://analytics.usa.gov */}
         <script src="https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js" id="_fed_an_ua_tag"></script>
