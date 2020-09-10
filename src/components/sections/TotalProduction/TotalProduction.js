1ccc011ce273e29427a69901defe1202f9423014
Jerome <jerome.louis@onrr.gov>
2020-09-10 09:52:42 -0600

    remove console log to rebuild

--- src/components/sections/TotalProduction/TotalProduction.js
+++ src/components/sections/TotalProduction/TotalProduction.js
@@ -85,7 +85,7 @@ const TotalProduction = props => {
   const [selected, setSelected] = useState(undefined)
 
   const toggleChange = value => {
-    // console.debug('ON TOGGLE CHANGE: ', value)
+
     setSelected(undefined)
     setToggle(value)
 
@@ -97,13 +97,11 @@ const TotalProduction = props => {
     }
   }
   const menuChange = value => {
-    // console.debug('ON Menu CHANGE: ', value)
     setSelected(undefined)
     setPeriod(value)
   }
 
   const handleSelect = value => {
-    // console.debug('handle select CHANGE: ', value)
     setSelected(value.selectedIndex)
   }
 
