e754fe1ce2496dd2f2fba353b1df436d959ed97c
Mojo Nichols <mojo.nichols@onrr.gov>
2020-09-30 08:40:36 -0600

    Added environment variable to config for hasura.  Adjusted skip option in load db to reflect actual line in file (-1 for header)

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,6 +1,7 @@
 
 import fetch from 'isomorphic-fetch'
 import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost'
+const HASURA_URI = (process.env.HASURA_URI) ? process.env.HASURA_URI : 'https://hasura-onrr.app.cloud.gov/v1/graphql'
 
 export const client = new ApolloClient({
 
@@ -9,7 +10,8 @@ export const client = new ApolloClient({
     // uri: 'https://hasura-onrr.app.cloud.gov/v1/graphql',
     // uri: 'https://hasura-sandbox.app.cloud.gov/v1/graphql',
     // uri: 'https://hasura-nrrd-a.app.cloud.gov/v1/graphql',
-    uri: 'https://hasura-nrrd-b.app.cloud.gov/v1/graphql',
+      // uri: 'https://hasura-nrrd-b.app.cloud.gov/v1/graphql',
+      uri: HASURA_URI,
     headers: {},
     fetch,
     resolvers: {}
