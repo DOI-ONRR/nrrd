7bd799ffe10e5d02e640cd8c55529eb9fb49fb88
Jennifer Malcolm <30806839+jennmalcolm@users.noreply.github.com>
2020-10-01 16:04:13 -0600

    update version

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ const config = {
       // eslint-disable-next-line max-len
       'This site provides open data about natural resource management on federal lands and waters in the United States, including oil, gas, coal, and other extractive industries.',
     googleAnalyticsId: GOOGLE_ANALYTICS_ID,
-    version: 'v6.2.0',
+    version: 'v6.2.1',
     author: '',
     dataRetrieval: {
       name: 'Data Specialists',
