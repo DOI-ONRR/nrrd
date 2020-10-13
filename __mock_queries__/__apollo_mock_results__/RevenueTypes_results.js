9afb12ca485a048dbbbc54b8aed17b3794d5ff78
Jerome <jerome.louis@onrr.gov>
2020-10-13 15:23:43 -0600

    init setup of active subMenu items

--- __mock_queries__/__apollo_mock_results__/RevenueTypes_results.js
+++
@@ -1,1 +0,0 @@
-export default { options: [{ option: 'Revenue' }, { option: 'Production' }, { option: 'Disbursements' }] }
--- __mock_queries__/data-filter-states.js
+++
@@ -1,8 +0,0 @@
-import {
-  DATA_TYPE,
-  REVENUE
-} from '../src/constants'
-
-export const DATA_FILTER_DEFAULTS_MOCK = 'dataFilterDefaultsMock'
-
-export const REVENUE_ONLY = { [DATA_FILTER_DEFAULTS_MOCK]: { [DATA_TYPE]: REVENUE } }
