394e12777d9b0ad74d4706ac9eb262bcbaa21c49
Jerome <jerome.louis@onrr.gov>
2020-10-14 16:03:26 -0600

    add check for dev

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,8 @@ require('dotenv').config({
 
 const GOOGLE_ANALYTICS_ID = (activeEnv === 'production') ? process.env.GOOGLE_ANALYTICS_ID : ''
 const GTM_ID = (activeEnv === 'production' || process.env.CIRCLE_BRANCH === 'dev') ? process.env.GTM_ID : ''
-const PATH_PREFIX = (activeEnv === 'production' || process.env.CIRCLE_BRANCH === 'dev') ? undefined : `/sites/${ process.env.CIRCLE_BRANCH }`
+// eslint-disable-next-line max-len
+const PATH_PREFIX = (activeEnv === 'production' || activeEnv === 'development' || process.env.CIRCLE_BRANCH === 'dev') ? undefined : `/sites/${ process.env.CIRCLE_BRANCH }`
 const HASURA_URI = process.env.HASURA_URI
 
 const config = {
