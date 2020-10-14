8db9a3e3aaf165e26bd2d12a8f2702d7671d1613
Jerome <jerome.louis@onrr.gov>
2020-10-14 16:30:35 -0600

    unstructured process env url

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -5,8 +5,7 @@ import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost'
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
-    // uri: process.env.HASURA_URI,
-    uri: 'https://hasura-onrr.app.cloud.gov/v1/graphql',
+    uri: process.env.HASURA_URI,
     headers: {},
     fetch,
     resolvers: {}
