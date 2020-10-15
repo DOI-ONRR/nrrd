195ed1d2237c8fdb578fe72f47ba07b9b08ad707
Jerome <jerome.louis@onrr.gov>
2020-10-15 15:07:00 -0600

    lint bustin'

--- src/components/data-viz/LineChart/D3LineChart.js
+++ src/components/data-viz/LineChart/D3LineChart.js
@@ -106,7 +106,6 @@ export default class D3LineChart {
       const lineDashes = this.lineDashes
       const lineStrokes = this.lineStrokes
       const lineTooltip = this.lineTooltip
-      const colors = this.chartColors
 
       const x = d3.scaleLinear().domain([2003, 2019]).range([0, width])
       // const y = d3.scaleLinear().domain([0, 40000000]).range([height, 0])
