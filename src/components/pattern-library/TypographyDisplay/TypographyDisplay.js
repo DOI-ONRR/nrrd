1eeb30e7c4d6d3cb60cdf9497b70153f14489327
Jeffery Keene <Jeff.Keene@master12studio.com>
2020-10-22 11:47:47 -0400

    added typography details

--- src/components/pattern-library/TypographyDisplay/TypographyDisplay.js
+++ src/components/pattern-library/TypographyDisplay/TypographyDisplay.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Grid from '@material-ui/core/Grid'
 import Box from '@material-ui/core/Box'
 import Typography from '@material-ui/core/Typography'
+import { ThemeProvider } from '@material-ui/core/styles'
 
 import theme from '../../../js/mui/theme'
 
@@ -18,11 +19,27 @@ const TypographyDisplay = () => {
             return (
               <Box key={key} m={2} p={1} border={1} borderColor='#e0e0e0'>
                 <Grid container direction="row" justify="flex-start" alignItems="stretch">
-                  <Grid item key={key} xs={12} sm={3} md={2}>
+                  <Grid item key={key} xs={12}>
                     <Box pl={2}>
-                      <Typography variant={key}>
-                        {key}
-                      </Typography>
+                      <Grid item key={key} xs={3}>
+                        <ThemeProvider theme={theme}>
+                          <Typography variant={key}>
+                            {key}
+                          </Typography>
+                        </ThemeProvider>
+                      </Grid>
+                      <Grid item key={key} xs={6}>
+                        {
+                          Object.keys(theme.typography[key]).map(subKey => {
+                            return (
+                              <Grid item xs={12}>
+                                <Typography color={'primary'} component={'span'}>{`${ subKey }: `}</Typography>
+                                <Typography component={'span'}>{`${ theme.typography[key][subKey] }`}</Typography>
+                              </Grid>
+                            )
+                          })
+                        }
+                      </Grid>
                     </Box>
                   </Grid>
                 </Grid>
