bb6a03b66e1d7d09fe37b9627b4c456e07d8f04c
Jeffery Keene <Jeff.Keene@master12studio.com>
2020-11-05 13:54:06 -0500

    wip

--- __mock_queries__/data-filter-states.js
+++ __mock_queries__/data-filter-states.js
@@ -5,4 +5,4 @@ import {
 
 export const DATA_FILTER_DEFAULTS_MOCK = 'dataFilterDefaultsMock'
 
-export const REVENUE_ONLY = { [DATA_FILTER_DEFAULTS_MOCK]: { [DATA_TYPE]: REVENUE } }
+export const REVENUE_DATA_TYPE_ONLY = { [DATA_FILTER_DEFAULTS_MOCK]: { [DATA_TYPE]: REVENUE } }
