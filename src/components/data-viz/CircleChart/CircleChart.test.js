d5ca3a05d9766af7f8d0ce64088567671ff7b65d
Mojo Nichols <mojo.nichols@onrr.gov>
2020-11-09 10:02:56 -0700

    Fixed lint issues

--- src/components/data-viz/CircleChart/CircleChart.test.js
+++ src/components/data-viz/CircleChart/CircleChart.test.js
@@ -7,85 +7,80 @@ import { render, screen } from 'test-utils'
 import CircleChart from './CircleChart'
 
 const chartData = [
-    {location_name: "Gulf of Mexico", total: 5163524881.620001},
-    {location_name: "New Mexico", total: 2447493889.24},
-    {location_name: "Wyoming", total: 1246326179.16},
-    {location_name: "Native American", total: 1155674819.2200003},
-    {location_name: "North Dakota", total: 289595600.7900001},
-    {location_name: "Other", total: 749011209.8500004},
-    {location_name: "Utah", total: 160480220.62999997},
-    {location_name: "California", total: 102385747.61},
-    {location_name: "Montana", total: 60166185.04999999},
-    {location_name: "Pacific", total: 50236044.75},
-    {location_name: "Alaska", total: 35739245.19},
-    {location_name: "Texas", total: 17681054.52},
-    {location_name: "Oklahoma", total: 17617606.7},
-    {location_name: "Louisiana", total: 9491872.040000001},
-    {location_name: "Nevada", total: 9412583.98},
-    {location_name: "Missouri", total: 9125090.67},
-    {location_name: "Idaho", total: 8593734.620000001},
-    {location_name: "Alaska Offshore", total: 8064335.08},
-    {location_name: "Atlantic", total: 4408728},
-    {location_name: "Ohio", total: 3478125.4},
-    {location_name: "Alabama", total: 3155672.3000000003},
-    {location_name: "Not tied to a lease", total: 2820634.97}
+  { location_name: 'Gulf of Mexico', total: 5163524881.620001 },
+  { location_name: 'New Mexico', total: 2447493889.24 },
+  { location_name: 'Wyoming', total: 1246326179.16 },
+  { location_name: 'Native American', total: 1155674819.2200003 },
+  { location_name: 'North Dakota', total: 289595600.7900001 },
+  { location_name: 'Other', total: 749011209.8500004 },
+  { location_name: 'Utah', total: 160480220.62999997 },
+  { location_name: 'California', total: 102385747.61 },
+  { location_name: 'Montana', total: 60166185.04999999 },
+  { location_name: 'Pacific', total: 50236044.75 },
+  { location_name: 'Alaska', total: 35739245.19 },
+  { location_name: 'Texas', total: 17681054.52 },
+  { location_name: 'Oklahoma', total: 17617606.7 },
+  { location_name: 'Louisiana', total: 9491872.040000001 },
+  { location_name: 'Nevada', total: 9412583.98 },
+  { location_name: 'Missouri', total: 9125090.67 },
+  { location_name: 'Idaho', total: 8593734.620000001 },
+  { location_name: 'Alaska Offshore', total: 8064335.08 },
+  { location_name: 'Atlantic', total: 4408728 },
+  { location_name: 'Ohio', total: 3478125.4 },
+  { location_name: 'Alabama', total: 3155672.3000000003 },
+  { location_name: 'Not tied to a lease', total: 2820634.97 }
 ]
-
-
 
 // The following are required for charts to render
 const chartTitle = 'Circle Chart Unit Test'
 const xAxis = 'location_name'
 const yAxis = 'total'
 
-
 describe('Circle Chart component:', () => {
   test('Basic Stacked Bar Chart  rendered succesfully', () => {
-      render(<CircleChart
+    render(<CircleChart
 		 data={chartData}
 		 title={chartTitle}
 	         xAxis={xAxis}
 	         yAxis={yAxis}
 
-      />)
-      expect(screen).toBeDefined() //Currently chart title is not put into div anywhere
-      //      expect(screen.getByText(chartTitle)).ToBeInTheDocument()
+    />)
+    expect(screen).toBeDefined() // Currently chart title is not put into div anywhere
+    //      expect(screen.getByText(chartTitle)).ToBeInTheDocument()
   })
 
   test('Basic Stacked Bar Chart CircleLabel', () => {
-      render(<CircleChart
+    render(<CircleChart
 		 data={chartData}
 	         xAxis={xAxis}
 	         yAxis={yAxis}
 		 circleLabel={
-                 d => {
-                     const r = []
-                     r[0] = 'foo'
-                     r[1] = 'bar'
-                     return r
-                 }
-                 }
-      
-      />)
-      expect(screen.getAllByText('foo')).toHaveLength(11)
+        d => {
+          const r = []
+          r[0] = 'foo'
+          r[1] = 'bar'
+          return r
+        }
+      }
 
+    />)
+    expect(screen.getAllByText('foo')).toHaveLength(11)
   })
-    
+
   test('Basic Stacked Bar Chart LegendLabel', () => {
-      render(<CircleChart
+    render(<CircleChart
 		 data={chartData}
 	         xAxis={xAxis}
 	         yAxis={yAxis}
-                 maxCircles={6}
-                 maxLegendWidth='800px'
+      maxCircles={6}
+      maxLegendWidth='800px'
 		 legendLabel={
-                 d => {
-                     return 'foo'
-                 }
-                 }
-      
-      />)
-      expect(screen.getAllByText('foo')).toHaveLength(6)
+        d => {
+          return 'foo'
+        }
+      }
 
+    />)
+    expect(screen.getAllByText('foo')).toHaveLength(6)
   })
 })
--- src/components/data-viz/Sparkline/Sparkline.test.js
+++ src/components/data-viz/Sparkline/Sparkline.test.js
@@ -6,40 +6,35 @@ import { render, screen } from 'test-utils'
 
 import Sparkline from './Sparkline'
 
-
-
-const chartData =[
-    ["2010", 4515762982.65],
-    ["2011", 6049822215.65],
-    ["2012", 6194230491.15],
-    ["2013", 8250817981.41],
-    ["2014", 80817981.41],
-    ["2015", 82817981.41],
-    ["2015", 4393132423.47],
-    ["2016", 2201599585.83],
-    ["2017", 2244876355.56],
-    ["2018", 3201250255.47],
-    ["2019", 4904041158.92]
+const chartData = [
+  ['2010', 4515762982.65],
+  ['2011', 6049822215.65],
+  ['2012', 6194230491.15],
+  ['2013', 8250817981.41],
+  ['2014', 80817981.41],
+  ['2015', 82817981.41],
+  ['2015', 4393132423.47],
+  ['2016', 2201599585.83],
+  ['2017', 2244876355.56],
+  ['2018', 3201250255.47],
+  ['2019', 4904041158.92]
 ]
-
 
 // The following are required for charts to render
 
-
 describe('Line Chart component:', () => {
-    test('Basic Sparkline Chart  rendered succesfully', () => {
-      render(<Sparkline
+  test('Basic Sparkline Chart  rendered succesfully', () => {
+    render(<Sparkline
 		 data={chartData}
-      />)
-      expect(screen).toBeDefined() 
+    />)
+    expect(screen).toBeDefined()
   })
-    test('Basic Sparkline Chart  highlight succesfully', () => {
+  test('Basic Sparkline Chart  highlight succesfully', () => {
 	  render(<Sparkline
 		     data={chartData}
 		     highlightIndex={4}
-	  
-	  />)
-	  expect(screen).toBeDefined() 
-      })
 
+	  />)
+	  expect(screen).toBeDefined()
+  })
 })
