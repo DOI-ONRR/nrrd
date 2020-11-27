194cf1d8567d633e31bbc051b9aa667cb7594ccf
Mojo Nichols <mojo.nichols@onrr.gov>
2020-11-27 09:52:58 -0700

    Used apollo to prevent build from using a massive amount of memory

--- src/components/DownloadDataFile/DownloadDataFile.js
+++ src/components/DownloadDataFile/DownloadDataFile.js
@@ -1,60 +1,66 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
-import { useStaticQuery, graphql } from 'gatsby'
+//import { useStaticQuery, graphql } from 'gatsby'
 import {EXCEL, CSV} from '../../constants'
 import { downloadWorkbook } from '../../js/utils'
 import Link from '../Link'
+import gql from 'graphql-tag'
+import { useQuery } from '@apollo/react-hooks'
 
-const DownloadDataFile = ( props ) => {
-    const data = useStaticQuery(graphql`	
-	query DownloadData {
-	    onrr {
-		fiscal_year_revenue: download_fiscal_year_revenue {
-		    commodity
-		    county
-		    district_type
-		    fips_code
-		    fiscal_year
-		    land_category
-		    land_class
-		    land_type
-		    location_name
-		    offshore_region
-		    period
-		    period_date
-		    product
-		    region_type
-		    revenue
-		    row_number
-		    state
-		    state_name
-		},
-		calendar_year_revenue: download_calendar_year_revenue {
-		    calendar_year
-		    period_date
-		    period
-		    region_type
-		    state
-		    state_name
-		    county
-		    district_type
-		    fips_code
-		    land_category
-		    land_class
-		    land_type
-		    location_name
-		    offshore_region
-		    commodity
-		    product
-		    revenue
-		    row_number
+const DOWNLOAD_REVENUE = gql`	
+    query DownloadData {
+	    fiscal_year_revenue: download_fiscal_year_revenue {
+		commodity
+		county
+		district_type
+		fips_code
+		fiscal_year
+		land_category
+		land_class
+		land_type
+		location_name
+		offshore_region
+		period
+		period_date
+		product
+		region_type
+		revenue
+		row_number
+		state
+		state_name
+	    },
+	    calendar_year_revenue: download_calendar_year_revenue {
+		calendar_year
+		period_date
+		period
+		region_type
+		state
+		state_name
+		county
+		district_type
+		fips_code
+		land_category
+		land_class
+		land_type
+		location_name
+		offshore_region
+		commodity
+		product
+		revenue
+		row_number
 
-		}
 	    }
-	}
-    `)
+    }
+`
+const DownloadDataFile = ( props ) => {
+    const { loading, error, data } = useQuery(DOWNLOAD_REVENUE)
 
     console.debug("Data: ",data)
+    if (loading) {
+	return 'Loading...'
+     }
+    
+    if (error) return `Error! ${ error.message }`
     
     const fileType=props.fileType
     //const fileName=props.fileName
@@ -69,8 +75,8 @@ const DownloadDataFile = ( props ) => {
     const handleDownload = () => {
 
 
-	const cols=Object.keys(data.onrr[fileBaseName][0]).map( (d,i)=>({title: d, name: d})) 
-	const rows=data.onrr[fileBaseName]
+	const cols=Object.keys(data[fileBaseName][0]).map( (d,i)=>({title: d, name: d})) 
+	const rows=data[fileBaseName]
 	console.debug("Handle download Here fileName here",cols, rows)
 	const r=downloadWorkbook(
             fileType,
@@ -82,11 +88,14 @@ const DownloadDataFile = ( props ) => {
 	
     }
     console.debug("LINK TYPE :", linkType)
-  return (
-      <Link linkType={linkType} href={'#' + fileName } onClick={() => handleDownload() } >
-	{props.children}
-      </Link>
-  )
+   if (data) {
+
+	return (
+	    <Link linkType={linkType} href={'#' + fileName } onClick={() => handleDownload() } >
+	      {props.children}
+	    </Link>
+	)
+   }else { return (null) }
 }
 
 export default DownloadDataFile
