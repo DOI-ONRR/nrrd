69c5e853508406113b7191b6d1b887c0cdaf41b4
master12 <Jeff.Keene@onrr.gov>
2021-01-19 21:51:35 -0500

    wip

--- src/components/pattern-library/ComponentDisplay/ComponentDisplay.js
+++ src/components/pattern-library/ComponentDisplay/ComponentDisplay.js
@@ -56,10 +56,7 @@ const ComponentDisplay = ({ children }) => {
   const url = (typeof window !== 'undefined') && new URL(window.location.href)
   // const type = url?.searchParams?.get('type')
 
-  const componentsInGroup = Object.keys(ALL_COMPONENTS).filter(i => {
-    console.log(i)
-    return true
-  })
+  const componentsInGroup = Object.keys(ALL_COMPONENTS)
 
   const components = results.allComponentMetadata.nodes.filter(item =>
     componentsInGroup.includes(item.displayName) &&
