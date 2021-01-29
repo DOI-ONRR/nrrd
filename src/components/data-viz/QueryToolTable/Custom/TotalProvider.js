a79485778b23bcfcae687f8f940b793471e7cff9
master12 <Jeff.Keene@onrr.gov>
2021-01-29 13:57:33 -0500

    updated query tool

--- src/components/data-viz/QueryToolTable/Custom/TotalProvider.js
+++ src/components/data-viz/QueryToolTable/Custom/TotalProvider.js
@@ -18,12 +18,12 @@ const TotalProvider = props => {
       ) {
         return ''
       }
-      return ((props.type === 'totalSum') ? 'Total:' : 'Subtotal:')
+      return ((props.type === 'totalSumLabel') ? 'Total:' : 'Subtotal:')
     }
 
     return (
       <>
-        {(props.type === 'totalSum' || props.type === 'sum')
+        {(props.type === 'totalSumLabel' || props.type === 'sumLabel')
           ? <div style={{ textAlign: 'right' }}>{getTotalLabel()}</div>
           : <div>{props.value}</div>
         }
