cdb08cd33c6cfd36da194be203f9753aad58dc64
Mojo Nichols <mojo.nichols@onrr.gov>
2021-02-24 18:38:27 -0700

    Debugging aws sync/copy

--- src/components/DownloadDataFile/DownloadDataFile.js
+++ src/components/DownloadDataFile/DownloadDataFile.js
@@ -11,16 +11,16 @@ const DownloadDataFile = (props) => {
     console.debug("childrin", props.children )
     console.debug("UTILS: ", utils)
     console.debug("childrin", props )
-    let  href='./downloads/'+props.dataSet
+    let  href='/downloads/'+props.dataSet
     let minDate=new Date(metaData[props.dataSet].minDate)
     let maxDate=new Date(metaData[props.dataSet].maxDate)
     let csvSize=Math.round(metaData[props.dataSet].csvSize/1000000)
     let xlsxSize=Math.round(metaData[props.dataSet].xlsxSize/1000000)
     return (<>
-      <Link href={href +'.xlsx'} >
+      <Link linkType="DownloadXls" href={href +'.xlsx'} >
 	{props.children + ', ' +minDate.getMonth()+1 +'/' +minDate.getFullYear() + ' - ' + maxDate.getMonth()+1 +'/' +maxDate.getFullYear() + ' (Excel, ' + xlsxSize + ' MB)'  }
       </Link>
-      <Link href={href +'.csv'} >
+      <Link linkType="DownloadCsv" href={href +'.csv'} >
 	{props.children + ', ' +minDate.getMonth()+1 +'/' +minDate.getFullYear() + ' - ' + maxDate.getMonth()+1 +'/' +maxDate.getFullYear() + ' (csv, ' + csvSize + ' MB)'  }
       </Link>
     </>
