821a98accbf437ab68195b7941c91ca52aeced19
Mojo Nichols <mojo.nichols@onrr.gov>
2021-03-12 12:23:00 -0700

    Added combined xlsx

--- src/components/DownloadDataFile/DownloadDataFile.js
+++ src/components/DownloadDataFile/DownloadDataFile.js
@@ -6,24 +6,22 @@ import Link from '../Link'
 import utils from '../../js/utils'
 
 const DownloadDataFile = props => {
-  console.debug('MetaData', metaData)
-  console.debug('childrin', props.children)
-  console.debug('UTILS: ', utils)
-  console.debug('childrin', props)
-  const href = './../' + props.dataSet
-  const minDate = new Date(metaData[props.dataSet].minDate)
-  const maxDate = new Date(metaData[props.dataSet].maxDate)
-  const csvSize = Math.round(metaData[props.dataSet].csvSize / 1000000)
-  const xlsxSize = Math.round(metaData[props.dataSet].xlsxSize / 1000000)
-  return (<>
-    <Link linkType="DownloadXls" href={href + '.xlsx'} >
-      {props.children + ', ' + minDate.getMonth() + 1 + '/' + minDate.getFullYear() + ' - ' + maxDate.getMonth() + 1 + '/' + maxDate.getFullYear() + ' (Excel, ' + xlsxSize + ' MB)' }
-    </Link>
-    <Link linkType="DownloadCsv" href={href + '.csv'} >
-      {props.children + ', ' + minDate.getMonth() + 1 + '/' + minDate.getFullYear() + ' - ' + maxDate.getMonth() + 1 + '/' + maxDate.getFullYear() + ' (csv, ' + csvSize + ' MB)' }
-    </Link>
-  </>
-  )
+    console.debug('MetaData', metaData)
+    console.debug('childrin', props.children)
+    console.debug('UTILS: ', utils)
+    console.debug('childrin', props)
+    const href = './../' + props.dataSet
+    const minDate = new Date(metaData[props.dataSet].minDate)
+    const maxDate = new Date(metaData[props.dataSet].maxDate)
+    const size = Math.round(metaData[props.dataSet].size / 1000000)
+
+    const suffix=(props.linkType === 'DownloadCsv') ? 'csv' : 'xlsx'
+    return (<>
+      <Link   linkType={props.linkType} href={ href + '.' + suffix } > 
+	{props.children + ', ' + minDate.getMonth() + 1 + '/' + minDate.getFullYear() + ' - ' + maxDate.getMonth() + 1 + '/' + maxDate.getFullYear() + ' (' + suffix +', ' + size + ' MB)' }
+      </Link>
+    </>
+    )
 }
 
 export default DownloadDataFile
