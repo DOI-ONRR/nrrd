cb096507883b544eca77d7bf66a828a958fa765d
Jerome <jerome.louis@onrr.gov>
2021-04-26 13:38:13 -0600

    adjust grid template row height

--- src/components/sections/ExploreData/DetailCards.js
+++ src/components/sections/ExploreData/DetailCards.js
@@ -148,7 +148,7 @@ const useStyles = makeStyles(theme => ({
       gridTemplateRows: '185px 770px 770px',
     },
     '& .cardContent__Disbursements': {
-      gridTemplateRows: '185px 900px 660px',
+      gridTemplateRows: '185px 950px 660px',
     },
     '& .cardContent__Production': {
       gridTemplateRows: '185px 325px 740px',
