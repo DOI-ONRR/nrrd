890401bc0112022476de2ba42fe75a5709d21c49
master12 <Jeff.Keene@onrr.gov>
2021-05-06 11:02:37 -0400

    fix images

--- src/components/images/IrsLogoImg.js
+++ src/components/images/IrsLogoImg.js
@@ -1,36 +1,19 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
-import Img from 'gatsby-image'
-/*
- * This component is built using `gatsby-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `useStaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
- * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.dev/gatsby-image
- * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import { GatsbyImage, StaticImage } from "gatsby-plugin-image"
 
 export default ({ alt, ...rest }) => {
   const data = useStaticQuery(graphql`
   query {
-      imageSharp(fluid: {originalName: {eq: "IRS-mark.png"}}) {
-        fluid {
-          ...GatsbyImageSharpFluid
+      file(relativePath: {eq: "logos/IRS-mark.png"}) {
+        childImageSharp {
+          gatsbyImageData
         }
       }
     }
   `)
-  if (!data.imageSharp) {
-    console.warn('IRS-mark.png did not load from graphql')
-    return <></>
-  }
+
   return (
-    <>
-      {data.imageSharp &&
-        <Img fluid={data.imageSharp.fluid} alt={alt || 'Internal Revenue Service'} {...rest} />
-      }
-    </>
+    <GatsbyImage image={data.file.childImageSharp.gatsbyImageData} alt={alt || 'Internal Revenue Service'} {...rest}/>
   )
 }
