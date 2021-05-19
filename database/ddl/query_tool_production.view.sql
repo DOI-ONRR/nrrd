eb28db93cc3c4565dddcb03a114b079dfae1f39a
Mojo Nichols <mojo.nichols@onrr.gov>
2021-05-19 15:32:31 -0600

    Got production working

--- database/ddl/query_tool_production.view.sql
+++ database/ddl/query_tool_production.view.sql
@@ -21,36 +21,65 @@ SET row_security = off;
 --
 
 CREATE VIEW public.query_tool_production AS
- SELECT location.location_name,
-    location.location_order,
-    location.land_type,
-    location.region_type,
-    location.district_type,
-    location.state_name,
-    location.county,
-    commodity.product,
-    period.period,
-    period.fiscal_year,
-    period.calendar_year,
-        CASE
-            WHEN ((period.month_long)::text <> ''::text) THEN (period.month_long)::text
-            ELSE NULL::text
-        END AS month_long,
-    (production.volume)::double precision AS production,
-        CASE
-            WHEN ((location.county)::text <> ''::text) THEN concat(location.state_name, ', ', location.county, ' ', location.district_type)
-            ELSE NULL::text
-        END AS county_name,
-        CASE
-            WHEN ((period.period)::text = 'Monthly'::text) THEN NULL::character varying
-            WHEN ((location.region_type)::text = 'County'::text) THEN location.state_name
-            WHEN ((location.region_type)::text = 'Offshore'::text) THEN (concat('Offshore ', location.location_name))::character varying
-            ELSE location.location_name
-        END AS state_offshore_name
-   FROM (((public.production
-     JOIN public.period USING (period_id))
-     JOIN public.location USING (location_id))
-     JOIN public.commodity USING (commodity_id));
+ SELECT _mview_location_qtp.g1,
+    _mview_location_qtp.g2,
+    _mview_location_qtp.g3,
+    _mview_location_qtp.location_name,
+    _mview_location_qtp.location_order,
+    _mview_location_qtp.land_type,
+    _mview_location_qtp.region_type,
+    _mview_location_qtp.district_type,
+    _mview_location_qtp.state_name,
+    _mview_location_qtp.county,
+    _mview_location_qtp.product,
+    _mview_location_qtp.period,
+    _mview_location_qtp.fiscal_year,
+    _mview_location_qtp.calendar_year,
+    _mview_location_qtp.month_long,
+    _mview_location_qtp.production,
+    _mview_location_qtp.county_name,
+    _mview_location_qtp.state_offshore_name
+   FROM public._mview_location_qtp
+UNION
+ SELECT _mview_commodity_qtp.g1,
+    _mview_commodity_qtp.g2,
+    _mview_commodity_qtp.g3,
+    _mview_commodity_qtp.location_name,
+    _mview_commodity_qtp.location_order,
+    _mview_commodity_qtp.land_type,
+    _mview_commodity_qtp.region_type,
+    _mview_commodity_qtp.district_type,
+    _mview_commodity_qtp.state_name,
+    _mview_commodity_qtp.county,
+    _mview_commodity_qtp.product,
+    _mview_commodity_qtp.period,
+    _mview_commodity_qtp.fiscal_year,
+    _mview_commodity_qtp.calendar_year,
+    _mview_commodity_qtp.month_long,
+    _mview_commodity_qtp.production,
+    _mview_commodity_qtp.county_name,
+    _mview_commodity_qtp.state_offshore_name
+   FROM public._mview_commodity_qtp
+UNION
+ SELECT _mview_location_commodity_qtp.g1,
+    _mview_location_commodity_qtp.g2,
+    _mview_location_commodity_qtp.g3,
+    _mview_location_commodity_qtp.location_name,
+    _mview_location_commodity_qtp.location_order,
+    _mview_location_commodity_qtp.land_type,
+    _mview_location_commodity_qtp.region_type,
+    _mview_location_commodity_qtp.district_type,
+    _mview_location_commodity_qtp.state_name,
+    _mview_location_commodity_qtp.county,
+    _mview_location_commodity_qtp.product,
+    _mview_location_commodity_qtp.period,
+    _mview_location_commodity_qtp.fiscal_year,
+    _mview_location_commodity_qtp.calendar_year,
+    _mview_location_commodity_qtp.month_long,
+    _mview_location_commodity_qtp.production,
+    _mview_location_commodity_qtp.county_name,
+    _mview_location_commodity_qtp.state_offshore_name
+   FROM public._mview_location_commodity_qtp;
 
 
 ALTER TABLE public.query_tool_production OWNER TO postgres;
