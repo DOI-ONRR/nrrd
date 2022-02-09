6209fed817c3245e2aa0f0f221fa14e9ec4c24ab
Mojo Nichols <mojo.nichols@onrr.gov>
2022-02-09 13:00:13 -0700

    Fixed dependabot issue;  Fixed bug in monthly production data

--- src/components/sections/TotalProduction/TotalProduction.js
+++ src/components/sections/TotalProduction/TotalProduction.js
@@ -117,7 +117,7 @@ const TotalProduction = props => {
   let maxCalendarYear
   let xGroups = {}
   let legendHeaders
-  const monthRange = []
+  let monthRange = []
   let startMonth
   let endMonth
   let monthRangeText
@@ -139,10 +139,10 @@ const TotalProduction = props => {
     maxCalendarYear = data.total_monthly_calendar_production.reduce((prev, current) => {
       return (prev.year > current.year) ? prev.year : current.year
     })
-    currentMonthNum = data.total_yearly_fiscal_production[data.total_yearly_fiscal_production.length - 1].currentMonth
+    currentMonthNum = data.total_monthly_fiscal_production[data.total_monthly_fiscal_production.length - 1].currentMonth
 
-    data.total_yearly_fiscal_production.filter(item => {
-      if (item.year === (maxFiscalYear + 1)) {
+    data.total_monthly_fiscal_production.filter(item => {
+      if (item.year === (maxFiscalYear)) {
         if (monthRange.indexOf(item.monthLong) === -1) monthRange.push(item.monthLong)
       }
     })
@@ -156,13 +156,26 @@ const TotalProduction = props => {
     // console.log('maxFiscalYear, maxCalendarYear: ', maxFiscalYear, maxCalendarYear)
 
     if (monthly === DFC.MONTHLY_CAPITALIZED) {
-      if (period === DFC.PERIOD_FISCAL_YEAR) {
+        if(monthRange.length < 12) {
+            maxFiscalYear--
+            maxCalendarYear--
+            monthRange=[]
+            data.total_monthly_fiscal_production.filter(item => {
+                if (item.year === (maxFiscalYear)) {
+                    if (monthRange.indexOf(item.monthLong) === -1) monthRange.push(item.monthLong)
+                }
+            })
+            
+        }
+        
+
+        if (period === DFC.PERIOD_FISCAL_YEAR) {
         comparisonData = data.total_monthly_fiscal_production.filter(row => row.product === product)
         chartData = data.total_monthly_fiscal_production.filter(row => row.product === product && row.year === maxFiscalYear)
       }
       else if (period === DFC.PERIOD_CALENDAR_YEAR) {
         comparisonData = data.total_monthly_calendar_production.filter(row => row.product === product)
-        chartData = data.total_monthly_calendar_production.filter(row => row.product === product && row.year === maxCalendarYear - 1)
+        chartData = data.total_monthly_calendar_production.filter(row => row.product === product && row.year === maxCalendarYear )
       }
       else {
         comparisonData = data.total_monthly_last_two_years_production.filter(row => row.product === product && row.year >= maxCalendarYear - 2)
@@ -170,7 +183,7 @@ const TotalProduction = props => {
 
 	  const minDate = new Date(maxDate.replace(/-/g, '/') + ' 00:00:00')
 	  minDate.setFullYear(minDate.getFullYear() - 1)
-	  // console.debug("MD------", minDate)
+	      //console.debug("MD------", minDate)
         chartData = data.total_monthly_last_twelve_production.filter(row => row.product === product && new Date(row.period_date) >= minDate)
       }
 
