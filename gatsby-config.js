9508c0e754f0669d98e96c95f1d754a3aab4666e
Faied <faiedm@mms.gov>
2022-10-28 08:30:46 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 const fetch = require('isomorphic-fetch')
 const { createHttpLink } = require('apollo-link-http')
 
-const activeEnv = (process.env.prd === 'master') ? 'prd' : 'dev'
+const activeEnv = (process.env.CIRCLE_BRANCH === 'master') ? 'prd' : 'dev'
 require('dotenv').config({
   path: `.env.${ activeEnv }`
 })
@@ -9,7 +9,6 @@ require('dotenv').config({
 const GOOGLE_ANALYTICS_ID = (activeEnv === 'prd') ? process.env.GOOGLE_ANALYTICS_ID : ''
 // eslint-disable-next-line max-len
 const PATH_PREFIX = (process.env.CIRCLE_STAGE === 'nrrd-preview') ? `/sites/${ process.env.CIRCLE_BRANCH }` : undefined
-const PATH_PREFIX = (process.env.CIRCLE_STAGE === 'master.env.prd')
 
 const config = {
   pathPrefix: PATH_PREFIX,
@@ -21,7 +20,7 @@ const config = {
       'This site provides open data about natural resource management on federal lands and waters in the United States, including oil, gas, coal, and other extractive industries.',
     keywords: 'Oil and gas, Coal, Renewable energy, Nonenergy minerals, Natural resource policy, Natural resource data, Extractives industries, Federal revenues, Production, 8(g) offshore revenue, offshore production, abanonded mine lands fund, mining reclamation tax, onrr state disbursement data, Native American land ownership, coal extraction, Department of the Interior, DOI, BLM coal leases, gomesa, gomesa funding, energy resource revenue, ONRR, state royalty, us eiti, solar industry, geothermal',
     googleAnalyticsId: GOOGLE_ANALYTICS_ID,
-    version: 'v6.7.0',
+    version: 'v6.6.8',
     author: '',
     dataRetrieval: {
       name: 'Data Specialists',
