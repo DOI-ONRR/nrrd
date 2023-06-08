620f144db285d9583724daac67be32abbdd10354
Jeff Schwartz <jeff.r.schwartz@gmail.com>
2023-06-08 11:33:09 -0400

    2524-alt-text-for-svgs: Adds title to stacked bar chart

--- src/components/data-viz/StackedBarChart/StackedBarChart2.js
+++ src/components/data-viz/StackedBarChart/StackedBarChart2.js
@@ -32,7 +32,8 @@ const StackedBarChart2 = ({ data, ...options }) => {
     yOrderBy,
     horizontal = false,
     collapsibleLegend,
-    collapsedLegend
+    collapsedLegend,
+    svgTitle
   } = options
   // console.log('StackedBarChart2 options: ', options)
   const theme = useTheme()
@@ -397,6 +398,7 @@ const StackedBarChart2 = ({ data, ...options }) => {
     <>
       {title && <ChartTitle compact={options.compact}>{title}</ChartTitle>}
       <svg width="100%" height="200" viewBox={`0 0 ${ viewBoxWidth } ${ viewBoxHeight }`} pointerEvents="none">
+        <title>{svgTitle}</title>
         <Translate>
           <MaxExtent
             extentPercent={extentPercent}
