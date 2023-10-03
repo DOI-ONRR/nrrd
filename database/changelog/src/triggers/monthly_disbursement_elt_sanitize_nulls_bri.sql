743d5710869f572eda913026e59b7c8ed5385a39
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2023-10-03 10:00:57 -0400

    2851-refactor-disbursements: Adds sanitize nulls

---
+++ database/changelog/src/triggers/monthly_disbursement_elt_sanitize_nulls_bri.sql
@@ -0,0 +1,7 @@
+DROP TRIGGER IF EXISTS monthly_disbursement_elt_sanitize_nulls_bri ON monthly_disbursement_elt;
+
+CREATE TRIGGER monthly_disbursement_elt_sanitize_nulls_bri
+    BEFORE INSERT
+    ON monthly_disbursement_elt
+    FOR EACH ROW
+    EXECUTE FUNCTION sanitize_nulls_disbursement;
