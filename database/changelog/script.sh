544f6e837fd47396bdd9acdd2518898d7ec5688f
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2023-10-16 08:44:34 -0400

    2903: Moves liquibase call to shell script.

---
+++ database/changelog/script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+liquibase update > liquibase.out &
+runfound=1
+errorfound=1
+while [ $runfound = 1 ] && [ $errorfound = 1 ];
+do 
+  grep Run: liquibase.out > /dev/null
+  runfound=$?
+  grep "Unexpected error" liquibase.out > /dev/null
+  errorfound=$?
+  sleep 1
+done
+updatecount=$(grep Run: liquibase.out | tr -s " " | cut -d ' ' -f2 | tr -d '\n')
+if [ $errorfound = 0 ] || [ "$updatecount" = 0 ]; then
+  echo "No changesets applied. Exiting job. errorfound ${errorfound}, updatecount ${updatecount}"
+  circleci-agent step halt
+fi
