79ec08df2f84a782ab3c853fdaae9a6c6fa805b5
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2023-10-25 19:31:14 -0400

    sept-monthly-uploads: Drops stored logic

--- database/changelog/src/functions/insert_location_disbursement.sql
+++ database/changelog/src/functions/insert_location_disbursement.sql
@@ -1,26 +1,2 @@
-CREATE OR REPLACE FUNCTION insert_location_disbursement()
-    RETURNS TRIGGER
-AS $$
-BEGIN
-    INSERT INTO location(
-        land_class,
-        land_category,
-        state,
-        county,
-        fips_code
-    )
-    VALUES(
-        CASE 
-            WHEN NEW.fund_type = 'Native American Tribes & Allottees' THEN 
-                'Native American' 
-            ELSE 'Federal' 
-        END,
-        COALESCE(NEW.land_category, ''),
-        COALESCE(NEW.state, ''),
-        COALESCE(NEW.county, ''),
-        COALESCE(NEW.fips_code, '')
-    )
-    ON CONFLICT DO NOTHING;
-
-    RETURN NULL;
-END $$ LANGUAGE PLPGSQL;
+DROP FUNCTION IF EXISTS insert_location_disbursement
+    CASCADE;
--- database/changelog/src/triggers/monthly_production_elt_insert_location_ari.sql
+++ database/changelog/src/triggers/monthly_production_elt_insert_location_ari.sql
@@ -1,7 +1,1 @@
 DROP TRIGGER IF EXISTS monthly_production_elt_insert_location_ari ON monthly_production_elt;
- 
-CREATE TRIGGER monthly_production_elt_insert_location_ari
-    AFTER INSERT
-    ON monthly_production_elt
-    FOR EACH ROW
-    EXECUTE FUNCTION insert_location_production();
