12165c67fedf5a465b3b969b5b45a614f4331403
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2023-10-30 08:34:26 -0400

    fyuploads: Reverts location trigger

--- database/changelog/src/triggers/monthly_disbursement_elt_insert_location_ari.sql
+++ database/changelog/src/triggers/monthly_disbursement_elt_insert_location_ari.sql
@@ -1,1 +1,7 @@
 DROP TRIGGER IF EXISTS monthly_disbursement_elt_insert_location_ari ON monthly_disbursement_elt;
+ 
+CREATE TRIGGER monthly_disbursement_elt_insert_location_ari
+    AFTER INSERT
+    ON monthly_disbursement_elt
+    FOR EACH ROW
+    EXECUTE FUNCTION insert_location_disbursement();
