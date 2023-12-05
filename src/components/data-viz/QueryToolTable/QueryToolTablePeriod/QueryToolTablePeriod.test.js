5d3ad0becd2f4009a558b1b6e0bd385d256d0210
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2023-12-04 20:30:05 -0500

    3022-sales-query-tool-sort: Addresses audit job errors

--- src/components/data-viz/QueryToolTable/QueryToolTablePeriod/QueryToolTablePeriod.test.js
+++ src/components/data-viz/QueryToolTable/QueryToolTablePeriod/QueryToolTablePeriod.test.js
@@ -4,21 +4,13 @@ import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import {
   render
-  // not used screen,
-	 // not used act
 } from 'test-utils'
 
 import QueryToolTable from './QueryToolTable'
 
-/* not used
- * import {
- *     QK_QUERY_TOOL
- * } from '../../../constants'
- *  */
-
 // Mocks the initial state of the AppStatusContext
-jest.mock('../../../stores/data-filter-store/reducers', () => {
-  const dataFilterState = require('../../../../../__mock_queries__/data-filter-states')
+jest.mock('../../../../stores/data-filter-store/reducers', () => {
+  const dataFilterState = require('../../../../../../__mock_queries__/data-filter-states')
   return ({
     get initialState () {
       return dataFilterState.QUERY_TOOL_TABLE_REVENUE
