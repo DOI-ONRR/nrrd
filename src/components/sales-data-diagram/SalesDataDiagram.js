48610e492f3223b4618300c42db1247a80f4afdc
Alexandra C McNally <122935097+AlexandracmONRR@users.noreply.github.com>
2024-02-28 10:35:07 -0500

    Add Quality and Gravity Bank glossary term and hover over
    I also added a break after accounting year definition on DD page.

--- src/components/sales-data-diagram/SalesDataDiagram.js
+++ src/components/sales-data-diagram/SalesDataDiagram.js
@@ -150,9 +150,9 @@ const SalesDataDiagram = () => {
                 depends on the location of the oil or gas lease. The location of the lease also determines the applicable statutory requirements.
               </Box>
               <Box my={'1rem'}>To determine the royalty due, reporters use the following steps:</Box>
-              <Box fontWeight={'bold'}>A. Calculate federal royalty value with adjustments for royalty relief, quality bank, and/or gravity bank</Box>
+              <Box fontWeight={'bold'}>A. Calculate federal royalty value with adjustments for royalty relief and/or quality and gravity bank adjustments</Box>
               <Box>
-                Revenues may be modified by various royalty relief programs, as well as quality bank and gravity bank adjustments. These modifications are made prior to
+                Revenues may be modified by various royalty relief programs, as well as <GlossaryTerm>quality and gravity bank adjustments</GlossaryTerm>. These modifications are made prior to
                 any allowances taking place and are included in the 2014 reporting. This calculation results in the Royalty Value Prior to Allowances (RVPA).
               </Box>
               <Box fontWeight={'bold'} mt={'1rem'}>B. Deduct regulatory allowances</Box>
