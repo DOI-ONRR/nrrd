bff10a9b06d127eb009585c72186bfed50a0b563
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2024-03-06 08:48:10 -0500

    3250: Merges database change commit

--- database/changelog/src/functions/transform_county_state_fips_code_prod_fy.sql
+++ database/changelog/src/functions/transform_county_state_fips_code_prod_fy.sql
@@ -11,27 +11,29 @@ BEGIN
         NEW.offshore_region := 'Gulf of Mexico';
     END IF;
 
-    DECLARE
-        v_fips_code county_lookup.fips_code%TYPE;
-    BEGIN
-        SELECT fips_code
-        INTO v_fips_code
-        FROM county_lookup
-        WHERE county = NEW.county
-            AND state = NEW.state;
+    IF NEW.fips_code IS NULL THEN
+        DECLARE
+            v_fips_code county_lookup.fips_code%TYPE;
+        BEGIN
+            SELECT fips_code
+            INTO v_fips_code
+            FROM county_lookup
+            WHERE county = NEW.county
+                AND state = NEW.state;
 
-        NEW.fips_code := v_fips_code;
-    EXCEPTION
-        WHEN NO_DATA_FOUND THEN
-            RAISE NOTICE 'No record in county_lookup. No problem.';
-    END;
+            NEW.fips_code := v_fips_code;
+        EXCEPTION
+            WHEN NO_DATA_FOUND THEN
+                RAISE NOTICE 'No record in county_lookup. No problem.';
+        END;
+    END IF;
 
     IF NEW.county = 'Santa Barbar' THEN
         NEW.county := 'Santa Barbara';
     END IF;
 
     IF NEW.county != '' THEN
-        NEW.fips_code := LPAD(fips_code, 5, '0');
+        NEW.fips_code := LPAD(NEW.fips_code, 5, '0');
     END IF;
 
     RETURN NEW;
