046766bc5c216c3bec88d8970e3d5f814ee10442
Alexandra C McNally <122935097+AlexandracmONRR@users.noreply.github.com>
2024-05-29 15:10:35 -0400

    State navigation - fix import button

--- src/components/data-by-state-buttons/DatabyStateButtons.js
+++ src/components/data-by-state-buttons/DatabyStateButtons.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Button from '@mui/material/Button';
+import { Button } from '@material-ui/core';
 
 const stateData = [
   { name: "Alabama", href: "https://revenuedata.doi.gov/explore/states/AL/" },
