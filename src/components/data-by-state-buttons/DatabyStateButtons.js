25ddb25bd719165e99c8a6d8e63ef48067f6b033
Alexandra C McNally <122935097+AlexandracmONRR@users.noreply.github.com>
2024-05-29 14:05:49 -0400

    Updated state navigation page mdx and js with flex buttons

--- src/components/data-by-state-buttons/DatabyStateButtons.js
+++ src/components/data-by-state-buttons/DatabyStateButtons.js
@@ -1,80 +1,80 @@
-// import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
-// import Button from '@mui/material/Button';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@mui/material/Button';
 
-// const stateData = [
-//   { name: "Alabama", href: "https://revenuedata.doi.gov/explore/states/AL/" },
-//   { name: "Alaska", href: "https://revenuedata.doi.gov/explore/states/AK/" },   
-//   { name: "Arkansas", href: "https://revenuedata.doi.gov/explore/states/AR/" },
-//   { name: "California", href: "https://revenuedata.doi.gov/explore/states/CA/" },
-//   { name: "Colorado", href: "https://revenuedata.doi.gov/explore/states/CO/" },
-//   { name: "Florida", href: "https://revenuedata.doi.gov/explore/states/FL/"},
-//   { name: "Idaho", href: "https://revenuedata.doi.gov/explore/states/ID/" }, 
-//   { name: "Illinois", href: "https://revenuedata.doi.gov/explore/states/IL/" },
-//   { name: "Indiana", href: "https://revenuedata.doi.gov/explore/states/IN/" },
-//   { name: "Kansas", href: "https://revenuedata.doi.gov/explore/states/KS/" },
-//   { name: "Louisiana", href: "https://revenuedata.doi.gov/explore/states/LA/" },
-//   { name: "Maryland", href: "https://revenuedata.doi.gov/explore/states/MD/" },
-//   { name: "Michigan", href: "https://revenuedata.doi.gov/explore/states/MI/" },
-//   { name: "Minnesota", href: "https://revenuedata.doi.gov/explore/states/MN/" },
-//   { name: "Mississippi", href: "https://revenuedata.doi.gov/explore/states/MS/" },
-//   { name: "Missouri", href: "https://revenuedata.doi.gov/explore/states/MO/" },
-//   { name: "Montana", href: "https://revenuedata.doi.gov/explore/states/MT/" },
-//   { name: "Nebraska", href: "https://revenuedata.doi.gov/explore/states/NE/" },
-//   { name: "Nevada", href: "https://revenuedata.doi.gov/explore/states/NV/" },
-//   { name: "New Mexico", href: "https://revenuedata.doi.gov/explore/states/NM/" },
-//   { name: "New York", href: "https://revenuedata.doi.gov/explore/states/NY/" },
-//   { name: "North Carolina", href: "https://revenuedata.doi.gov/explore/states/NC/" },
-//   { name: "North Dakota", href: "https://revenuedata.doi.gov/explore/states/ND/" },
-//   { name: "Ohio", href: "https://revenuedata.doi.gov/explore/states/OH/" },
-//   { name: "Oklahoma", href: "https://revenuedata.doi.gov/explore/states/OK/" },
-//   { name: "Oregon", href: "https://revenuedata.doi.gov/explore/states/OR/" },
-//   { name: "Pennsylvania", href: "https://revenuedata.doi.gov/explore/states/PA/" },
-//   { name: "South Carolina", href: "https://revenuedata.doi.gov/explore/states/SC/" },
-//   { name: "South Dakota", href: "https://revenuedata.doi.gov/explore/states/SD/" },
-//   { name: "Texas", href: "https://revenuedata.doi.gov/explore/states/TX/" },
-//   { name: "Utah", href: "https://revenuedata.doi.gov/explore/states/UT/" },
-//   { name: "Virginia", href: "https://revenuedata.doi.gov/explore/states/VA/" },
-//   { name: "Washington", href: "https://revenuedata.doi.gov/explore/states/WA/" },
-//   { name: "West Virginia", href: "https://revenuedata.doi.gov/explore/states/WV/" },
-//   { name: "Wisconsin", href: "https://revenuedata.doi.gov/explore/states/WI/" },
-//   { name: "Wyoming", href: "https://revenuedata.doi.gov/explore/states/WY/" },
-//   { name: "Alaska Offshore Region", href: "https://revenuedata.doi.gov/explore/offshore-regions/alaska/" },
-//   { name: "Atlantic Offshore Region", href: "https://revenuedata.doi.gov/explore/offshore-regions/atlantic/"},
-//   { name: "Gulf Offshore Region", href: "https://revenuedata.doi.gov/explore/offshore-regions/gulf/"},
-//   { name: "Pacific Offshore Region", href: "https://revenuedata.doi.gov/explore/offshore-regions/pacific/"}
-// ];
+const stateData = [
+  { name: "Alabama", href: "https://revenuedata.doi.gov/explore/states/AL/" },
+  { name: "Alaska", href: "https://revenuedata.doi.gov/explore/states/AK/" },   
+  { name: "Arkansas", href: "https://revenuedata.doi.gov/explore/states/AR/" },
+  { name: "California", href: "https://revenuedata.doi.gov/explore/states/CA/" },
+  { name: "Colorado", href: "https://revenuedata.doi.gov/explore/states/CO/" },
+  { name: "Florida", href: "https://revenuedata.doi.gov/explore/states/FL/"},
+  { name: "Idaho", href: "https://revenuedata.doi.gov/explore/states/ID/" }, 
+  { name: "Illinois", href: "https://revenuedata.doi.gov/explore/states/IL/" },
+  { name: "Indiana", href: "https://revenuedata.doi.gov/explore/states/IN/" },
+  { name: "Kansas", href: "https://revenuedata.doi.gov/explore/states/KS/" },
+  { name: "Louisiana", href: "https://revenuedata.doi.gov/explore/states/LA/" },
+  { name: "Maryland", href: "https://revenuedata.doi.gov/explore/states/MD/" },
+  { name: "Michigan", href: "https://revenuedata.doi.gov/explore/states/MI/" },
+  { name: "Minnesota", href: "https://revenuedata.doi.gov/explore/states/MN/" },
+  { name: "Mississippi", href: "https://revenuedata.doi.gov/explore/states/MS/" },
+  { name: "Missouri", href: "https://revenuedata.doi.gov/explore/states/MO/" },
+  { name: "Montana", href: "https://revenuedata.doi.gov/explore/states/MT/" },
+  { name: "Nebraska", href: "https://revenuedata.doi.gov/explore/states/NE/" },
+  { name: "Nevada", href: "https://revenuedata.doi.gov/explore/states/NV/" },
+  { name: "New Mexico", href: "https://revenuedata.doi.gov/explore/states/NM/" },
+  { name: "New York", href: "https://revenuedata.doi.gov/explore/states/NY/" },
+  { name: "North Carolina", href: "https://revenuedata.doi.gov/explore/states/NC/" },
+  { name: "North Dakota", href: "https://revenuedata.doi.gov/explore/states/ND/" },
+  { name: "Ohio", href: "https://revenuedata.doi.gov/explore/states/OH/" },
+  { name: "Oklahoma", href: "https://revenuedata.doi.gov/explore/states/OK/" },
+  { name: "Oregon", href: "https://revenuedata.doi.gov/explore/states/OR/" },
+  { name: "Pennsylvania", href: "https://revenuedata.doi.gov/explore/states/PA/" },
+  { name: "South Carolina", href: "https://revenuedata.doi.gov/explore/states/SC/" },
+  { name: "South Dakota", href: "https://revenuedata.doi.gov/explore/states/SD/" },
+  { name: "Texas", href: "https://revenuedata.doi.gov/explore/states/TX/" },
+  { name: "Utah", href: "https://revenuedata.doi.gov/explore/states/UT/" },
+  { name: "Virginia", href: "https://revenuedata.doi.gov/explore/states/VA/" },
+  { name: "Washington", href: "https://revenuedata.doi.gov/explore/states/WA/" },
+  { name: "West Virginia", href: "https://revenuedata.doi.gov/explore/states/WV/" },
+  { name: "Wisconsin", href: "https://revenuedata.doi.gov/explore/states/WI/" },
+  { name: "Wyoming", href: "https://revenuedata.doi.gov/explore/states/WY/" },
+  { name: "Alaska Offshore Region", href: "https://revenuedata.doi.gov/explore/offshore-regions/alaska/" },
+  { name: "Atlantic Offshore Region", href: "https://revenuedata.doi.gov/explore/offshore-regions/atlantic/"},
+  { name: "Gulf Offshore Region", href: "https://revenuedata.doi.gov/explore/offshore-regions/gulf/"},
+  { name: "Pacific Offshore Region", href: "https://revenuedata.doi.gov/explore/offshore-regions/pacific/"}
+];
 
-// const useStyles = makeStyles((theme) =>({
-//   root: {
-//     display: 'flex',
-//     flexWrap: 'wrap',
-//     justifyContent: 'center',
-//     gap: theme.spacing(2),
-//   },
-//   button: {
-//     minWidth: '200px',
-//   },
-// }));
+const useStyles = makeStyles((theme) =>({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    gap: theme.spacing(2),
+  },
+  button: {
+    minWidth: '200px',
+  },
+}));
 
-// const DatabyStateButtons = ({stateData})> => {
-//   const classes = useStyles ();
+const DatabyStateButtons = ({stateData}) => {
+  const classes = useStyles ();
 
-//   return (
-//     <div className={classes.root}>
-//       {stateData.map((state, index) => (
-//         <Button
-//           key={index}
-//           className={classes.button}
-//           variant="contained"
-//           color="primary"
-//           href={state.href}
-//         >
-//           {state.name}
-//         </Button>
-//       ))}
-//     </div>
-//   );
-// }
+  return (
+    <div className={classes.root}>
+      {stateData.map((state, index) => (
+        <Button
+          key={index}
+          className={classes.button}
+          variant="contained"
+          color="primary"
+          href={state.href}
+        >
+          {state.name}
+        </Button>
+      ))}
+    </div>
+  );
+}
 
-// export default DatabyStateButtons
+export default DatabyStateButtons
