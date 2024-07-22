b116810fd29d9e98e681a3138bd2fb8de74e1d0f
Alexandra C McNally <122935097+AlexandracmONRR@users.noreply.github.com>
2024-07-22 13:34:18 -0400

    Added AZ to the list in Data By State Buttons

--- src/components/data-by-state-buttons/DatabyStateButtons.js
+++ src/components/data-by-state-buttons/DatabyStateButtons.js
@@ -6,6 +6,7 @@ const stateData = [
   { name: 'Alabama', href: 'https://revenuedata.doi.gov/explore/states/AL/' },
   { name: 'Alaska', href: 'https://revenuedata.doi.gov/explore/states/AK/' },
   { name: 'Arkansas', href: 'https://revenuedata.doi.gov/explore/states/AR/' },
+  { name: 'Arizona', href: 'https://revenuedata.doi.gov/explore/states/AZ/' },
   { name: 'California', href: 'https://revenuedata.doi.gov/explore/states/CA/' },
   { name: 'Colorado', href: 'https://revenuedata.doi.gov/explore/states/CO/' },
   { name: 'Florida', href: 'https://revenuedata.doi.gov/explore/states/FL/' },
