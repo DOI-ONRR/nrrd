f1725ed717798670885696303c700fbbb55fbcda
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2024-12-16 16:20:31 -0500

    3694: Update fact sheet banner

--- src/components/info/SnapshotBanner/SnapshotBanner.js
+++ src/components/info/SnapshotBanner/SnapshotBanner.js
@@ -6,21 +6,30 @@ import useTheme from '@material-ui/styles/useTheme'
 import Box from '@material-ui/core/Box'
 import Grid from '@material-ui/core/Grid'
 
-import { OnrrLogoImg } from '../../images'
-// not used import { Typography } from '@material-ui/core'
 import Link from '../../Link'
 import PrintIcon from '@material-ui/icons/Print'
 import IconButton from '@material-ui/core/IconButton'
+import { Button } from '@material-ui/core'
 
 const useStyles = makeStyles(theme => ({
   root: {
-    backgroundColor: theme.palette.toolbarBackground.main,
     marginTop: theme.spacing(3),
-    '@media print': {
-      backgroundColor: theme.palette.toolbarBackground.main,
-      '-webkit-print-color-adjust': 'exact'
-    }
   },
+  printButton: {
+    backgroundColor: theme.palette.toolbarBackground.main,
+    fontSize: '1rem',
+    lineHeight: '1rem',
+    color: 'white',
+    textAlign: 'end'
+  },
+  dataPublished: {
+    fontSize: '1rem',
+    lineHeight: '1rem',
+    textAlign: 'end',
+    marginTop: '1rem',
+    marginBottom: '1rem',
+    fontStyle: 'italic'
+  }
 }))
 /**
  * This banner is used for the Monthly Snapshot on the fact sheet page
@@ -32,36 +41,33 @@ const SnapshotBanner = () => {
   return (
     <Box className={classes.root} displayPrint="block">
       <Grid container>
-        <Grid item xs={6}>
-          <Box ml={1}>
+        <Grid item xs={8}>
+          <Box>
             <Grid container alignItems="center">
-              <Box component='span' mr={1} mt={'5px'}>
-                <OnrrLogoImg style={{ height: '50px', width: '50px' }} />
-              </Box>
               <Box component='span'>
-                <Box style={{ fontSize: '0.7rem', lineHeight: '0.7rem', color: 'white' }}>
-                  U.S. Department of the Interior
+                <Box style={{ fontSize: '1rem' }}>
+                  U.S. Department of the Interior, Natural Resources Revenue Data
                 </Box>
-                <Box style={{ fontSize: '1rem', lineHeight: '1rem', fontWeight: '600', color: 'white', width: '150px', marginTop: '5px' }}>
-                  Natural Resources Revenue Data
+                <Box style={{ fontSize: '2.25rem', fontWeight: '600', marginTop: '0.5rem' }}>
+                  Monthly Fact Sheet
                 </Box>
               </Box>
             </Grid>
           </Box>
         </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={4} style={{ textAlign: 'right' }}>
           <Grid container>
             <Grid item xs={12}>
-              <Box m={1} mr={2} style={{ fontSize: '1.1rem', lineHeight: '1.1rem', color: 'white', textAlign: 'end' }}>
+              <Button variant="contained" className={classes.printButton} m={1}>
                 <IconButton size="small" color='inherit' aria-label="print" component="span" onClick={() => window.print()} style={{ marginRight: '5px' }}>
                   <PrintIcon />
                 </IconButton>
-                Monthly Fact Sheet
-              </Box>
+                Print Fact Sheet
+              </Button>
             </Grid>
             <Grid item xs={12}>
-              <Box style={{ fontSize: '0.9rem', lineHeight: '0.9rem', color: 'white', textAlign: 'end' }} mb={1} mr={2}>
-                View interactive charts at <Link href={'https://revenuedata.doi.gov/'} style={{ color: 'white' }}>revenuedata.doi.gov</Link>
+              <Box className={classes.dataPublished}>
+                Data published November 2024
               </Box>
             </Grid>
           </Grid>
