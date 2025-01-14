d05057b23e30c0af07c5cef6ffd852632858dcdc
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2025-01-14 09:56:43 -0500

    3392: Moves connectToDevTools

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -7,9 +7,9 @@ export const client = new ApolloClient({
   link: new HttpLink({
     uri: process.env.GATSBY_HASURA_URI,
     // uri: 'http://localhost:8080/v1/graphql',
-    connectToDevTools: true,
     headers: {},
     fetch,
     resolvers: {}
   }),
+  connectToDevTools: true,
 })
