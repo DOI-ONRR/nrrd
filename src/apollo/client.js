5766b7ad343acaee6d18a95bb403528d40f2ab7f
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2025-01-14 09:36:48 -0500

    3392: enable Apollo dev tools

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -6,6 +6,8 @@ export const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
     uri: process.env.GATSBY_HASURA_URI,
+    // uri: 'http://localhost:8080/v1/graphql',
+    connectToDevTools: true,
     headers: {},
     fetch,
     resolvers: {}
