785f6a0c1d7f2495fd7967d1230976210b99b080
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2025-01-25 11:14:48 -0500

    Feb-25-Uploads: Updates NA revenue proc

--- database/changelog/src/procedures/summarize_native_american_revenue.sql
+++ database/changelog/src/procedures/summarize_native_american_revenue.sql
@@ -13,7 +13,7 @@ BEGIN
         mineral_production_code_desc,
         commodity,
         product_code_desc,
-        SUM(TO_NUMBER(revenue, 'L999G999G999G999D999999999')) revenue
+        SUM(revenue) revenue
     FROM monthly_revenue_elt 
     WHERE land_class_code LIKE '%Indian%' 
     GROUP BY accept_date, 
