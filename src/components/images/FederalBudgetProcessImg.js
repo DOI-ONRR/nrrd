4945d5c5295fc344a950d1dceaa031d52514b112
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2025-03-13 08:36:50 -0400

    3804: Fixes image dimensions

--- src/components/images/FederalBudgetProcessImg.js
+++ src/components/images/FederalBudgetProcessImg.js
@@ -19,11 +19,20 @@ export default ({ alt, ...rest }) => {
     return <></>
   }
 
+  const altText = 'Federal budget process. First, statute (federal statutes determine the maximum amount of funds that can be appropriated). ' + 
+  'Second, appropriation (Congress determines the amount that a given entity or agency will receive). ' +
+  'Third, grant (recipient entities determine how much the funding will be allocated for use in their budgets). ' +
+  'Finally, disbursement (recipient entities disburse funds over the course of the fiscal year for budgeted purposes).';
+
   return (
     <>
       {data.file.childImageSharp.gatsbyImageData &&
       // eslint-disable-next-line max-len
-      <GatsbyImage image={data.file.childImageSharp.gatsbyImageData} alt={alt || 'Federal budget process. First, statute (federal statutes determine the maximum amount of funds that can be appropriated). Second, appropriation (Congress determines the amount that a given entity or agency will receive). Third, grant (recipient entities determine how much the funding will be allocated for use in their budgets). Finally, disbursement (recipient entities disburse funds over the course of the fiscal year for budgeted purposes).'} {...rest} />
+      <GatsbyImage 
+        image={data.file.childImageSharp.gatsbyImageData} 
+        alt={alt || altText} 
+        style={{ width: '440px' }} 
+        {...rest} />
       }
     </>
   )
