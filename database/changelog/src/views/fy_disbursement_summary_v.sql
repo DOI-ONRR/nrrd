da927a1f0246c0fb3bd1a7abe0a9d529b8ad04c1
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2025-07-08 12:33:43 -0400

    4031: Adds disbursement view

---
+++ database/changelog/src/views/fy_disbursement_summary_v.sql
@@ -0,0 +1,47 @@
+CREATE OR REPLACE VIEW fy_disbursement_summary_v AS
+SELECT SUM(disbursement) disbursement,
+  'current' fy
+FROM disbursement d,
+  period pe
+WHERE pe.period_id = d.period_id
+  AND pe.period = 'Monthly'
+  AND pe.fiscal_year = (
+    SELECT max(fiscal_year)
+    FROM period pe2
+    WHERE exists (
+      SELECT 1
+      FROM revenue
+      WHERE period_id = pe2.period_id
+    )
+  )
+GROUP BY fy
+UNION
+SELECT SUM(disbursement) disbursement,
+  'previous' fy
+FROM disbursement d,
+  period pe
+WHERE pe.period_id = d.period_id
+  AND pe.period = 'Monthly'
+  AND pe.fiscal_year = (
+    SELECT max(fiscal_year) - 1
+    FROM period pe2
+    WHERE exists (
+      SELECT 1
+      FROM revenue
+      WHERE period_id = pe2.period_id
+    )
+  )
+  AND pe.fiscal_month <= (
+    SELECT max(fiscal_month)
+    FROM period pe2
+    WHERE fiscal_year = (
+      SELECT max(fiscal_year)
+      FROM period pe3
+      WHERE exists (
+        SELECT 1
+        FROM production
+        WHERE period_id = pe2.period_id
+      )
+    )
+  )
+GROUP BY fy;
