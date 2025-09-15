6c5ee16adf3ecf0ae14f62ba0d57a0c31054b92e
lpgoldstein <lindsay.goldstein@onrr.gov>
2025-09-15 13:09:48 -0600

    Update ReleaseDetails.js

--- src/components/sections/ReleaseDetails/ReleaseDetails.js
+++ src/components/sections/ReleaseDetails/ReleaseDetails.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Box, Typography, Link, List, ListItem, ListItemText } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
   h3: {
     marginTop: '0.5rem',
-    marginBottom: '1rem'
+    marginBottom: '1rem',
   },
   changes: {
     display: 'block',
-    marginTop: '1rem'
+    marginTop: '1rem',
   },
   changeList: {
     listStyleType: 'disc',
@@ -23,67 +23,73 @@ const useStyles = makeStyles({
     },
     '& .MuiListItemText-primary': {
       fontSize: '1rem',
-    }
-  }
-})
+    },
+  },
+});
 
 export default function ReleaseDetails() {
   const classes = useStyles();
-
   return (
-    <Box mb={4} pt={0.5} pb={2} pl={3} pr={3} borderRadius={10} border={'1px solid #3C3D3E'}>
-      <Typography variant='h3' className={classes.h3}>
-        Latest release details
-      </Typography>
-
-      <Typography variant="inherit" className={classes.changes}>
-        September 16, 2025 changes:
-      </Typography>
-
-      <List className={classes.changeList}>
-        <ListItem>
-          <ListItemText
-            primary={
-              <>
-              Added <Link href="downloads/disbursements-by-month/" linkType="default">monthly disbursements data</Link>
-              </>
-            }
-          />
-        </ListItem>
-        <ListItem>
-        <ListItemText
-          primary={
-            <>
-            Added <Link href="/downloads/production-by-month/" linkType="default">monthly production data  </Link>
-            </>
-          }
-        />
-      </ListItem>
-      <ListItem>
-        <ListItemText
-          primary={
-            <>
-           Added <Link href="downloads/revenue/" linkType="default">monthly revenue data</Link>
-            </>
-          }
-        />
-      </ListItem>
-      <ListItem>
-        <ListItemText
-          primary={
-            <>
-            Added <Link href="downloads/production-by-disposition/" linkType="default"> monthly production disposition data
-            </Link>
-            </>
-          }
-        />
-      </ListItem>
-            
-      </List>
-
-      <Typography variant='inherit'>
-        Review our <Link href="https://github.com/ONRR/nrrd/releases">full release details</Link>.
-      </Typography>
-    </Box>
-  )
+    <>
+      <Box mb={4} pt={0.5} pb={2} pl={3} pr={3} borderRadius={10} border={'1px solid #3C3D3E'}>
+        <Typography variant="h3" className={classes.h3}>
+          Latest release details
+        </Typography>
+        <Typography variant="inherit" className={classes.changes}>
+          September 16, 2025 changes:
+        </Typography>
+        <List className={classes.changeList}>
+          <ListItem>
+            <ListItemText
+              primary={
+                <>
+                  Added <Link href="downloads/disbursements-by-month/">monthly disbursements data</Link>
+                </>
+              }
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              primary={
+                <>
+                  Added <Link href="/downloads/production-by-month/">monthly production data</Link>
+                </>
+              }
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              primary={
+                <>
+                  Added <Link href="downloads/revenue/">monthly revenue data</Link>
+                </>
+              }
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              primary={
+                <>
+                  Added <Link href="downloads/production-by-disposition/">monthly production disposition data</Link>
+                </>
+              }
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemText
+              primary={
+                <>
+                  Added <Link href="how-revenue-works/coal-excise-tax/">coal excise tax</Link>
+                </>
+              }
+            />
+          </ListItem>
+        </List>
+        <Typography variant="inherit">
+          Review our{' '}
+          <Link href="https://github.com/ONRR/nrrd/releases">full release details</Link>.
+        </Typography>
+      </Box>
+    </>
+  );
 }
