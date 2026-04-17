56e41b22c12d18409080a551dc78e59ab3429809
lpgoldstein <lindsay.goldstein@onrr.gov>
2026-04-17 13:47:35 -0600

    Update FactSheetBanner.js

--- src/components/FactSheet/FactSheetBanner.js
+++ src/components/FactSheet/FactSheetBanner.js
@@ -71,7 +71,7 @@ const FactSheetBanner = () => {
             </Grid>
             <Grid item xs={12}>
               <Box className={classes.dataPublished}>
-                Data published March 2026
+                Data published April 2026
               </Box>
             </Grid>
           </Grid>
