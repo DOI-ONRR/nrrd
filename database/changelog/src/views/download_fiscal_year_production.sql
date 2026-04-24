064cdb970a167646438cdc9f128afd15c544bd8a
Jeff Schwartz <jeffrey.schwartz@onrr.gov>
2026-04-24 12:59:14 -0400

    dev: updates fy production download view

---
+++ database/changelog/src/views/download_fiscal_year_production.sql
@@ -0,0 +1,22 @@
+DROP VIEW IF EXISTS download_fiscal_year_production;
+
+CREATE VIEW download_fiscal_year_production AS
+SELECT DISTINCT period.period_date AS "Date",
+  period.fiscal_year AS "Fiscal Year",
+  location.land_class AS "Land Class",
+  location.land_category AS "Land Category",
+  location.state_name AS "State",
+  location.county AS "County",
+  CASE
+    WHEN location.county::text <> ''::text THEN location.fips_code
+    ELSE ''::character varying
+  END AS "FIPS Code",
+  location.offshore_region AS "Offshore Region",
+  commodity.product AS "Product",
+  production.volume AS "Volume"
+FROM production
+  JOIN period USING (period_id)
+  JOIN location USING (location_id)
+  JOIN commodity USING (commodity_id)
+WHERE period.period::text = 'Fiscal Year'::text
+ORDER BY period.period_date;
