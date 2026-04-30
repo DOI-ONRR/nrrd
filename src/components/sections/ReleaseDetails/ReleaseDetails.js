3e668568622488846612fab05e38f27722f3cc58
lpgoldstein <lindsay.goldstein@onrr.gov>
2026-04-30 07:18:29 -0600

    Update release date and version

--- src/components/sections/ReleaseDetails/ReleaseDetails.js
+++ src/components/sections/ReleaseDetails/ReleaseDetails.js
@@ -37,7 +37,7 @@ export default function ReleaseDetails() {
           Latest release details
         </Typography>
         <Typography variant="inherit" className={classes.changes}>
-          April 23, 2026 changes:
+          April 30, 2026 changes:
         </Typography>
         <List className={classes.changeList}>
         <ListItem>
